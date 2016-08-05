75b38a0bfc8f13a57cb4d33fc652e689172fdfe8
yanwenyuan <yanwenyuan@bbdservice.com>
2016-08-05 17:24:03 +0800

    注释规范

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ParkController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ParkController.java
@@ -1,54 +1,45 @@
 package com.bbd.wtyh.web.controller;
 
-import java.util.List;
-
+import com.bbd.wtyh.entity.Area;
+import com.bbd.wtyh.service.AreaService;
+import com.bbd.wtyh.web.ResponseBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.bbd.wtyh.common.ResponseBean;
-import com.bbd.wtyh.entity.Area;
-import com.bbd.wtyh.service.AreaService;
-
+import java.util.List;
 
 
 /**
-* @ClassName: ParkController
-* @Description: 园区相关接口
-* @author Ian.Su
-* @date 2016年8月5日 上午11:55:20
-*/
+ * 园区相关接口
+ *
+ * @author Ian.Su
+ * @since: 2016年8月5日 上午11:55:20
+ */
 @Controller
 @RequestMapping("/park")
 public class ParkController {
 
+    //  属性描述
+    private int test = 1;
     @Autowired
     private AreaService areaService;
 
 
-    
     /**
-    *
-    * @Title: 获取区域信息
-    * @Description: TODO
-    * @param   设定文件
-    * @return Object    返回类型
-    * @throws
-    * @author Ian.Su
-    * @date 2016年8月5日 下午2:44:35 
-    */
+     * 方法描述
+     *
+     * @return Object    返回类型
+     */
     @RequestMapping("/area/list")
     @ResponseBody
     public Object areaList() {
 
         List<Area> data = areaService.list();
-        
+
         return ResponseBean.successResponse(data);
     }
-    
-    
-    
-    
-    
+
+
 }
