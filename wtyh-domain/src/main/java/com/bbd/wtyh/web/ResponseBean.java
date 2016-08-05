a61fb47d6845367bf64692193f2f3354606ecac9
yanwenyuan <yanwenyuan@bbdservice.com>
2016-08-05 14:57:02 +0800

    1 增加 page类 2 增加 ResponseBean

--- wtyh-domain/src/main/java/com/bbd/wtyh/web/ResponseBean.java
+++ wtyh-domain/src/main/java/com/bbd/wtyh/web/ResponseBean.java
@@ -2,85 +2,64 @@ package com.bbd.wtyh.web;
 
 
 public class ResponseBean {
-	
-	private boolean success;
-	
-	private Object content;
-	
-	private Object msg;
-	
-	
-	
-	/**
-	 * @param content 成功的内容
-	 * @return
-	 * @author Ian.Su
-	 */
-	public static ResponseBean successResponse(Object content){
-		
-		ResponseBean responseBean = new ResponseBean();
-		
-		responseBean.success = true;
-		
-		responseBean.content = content;
-		
-		return responseBean;
-	}
-	
-	
-	
-	
-	/**
-	 * @param msg 失败的内容，比如提示语句
-	 * @return
-	 * @author Ian.Su
-	 */
-	public static ResponseBean errorResponse(Object msg){
-		
-		ResponseBean responseBean = new ResponseBean();
-		
-		responseBean.success = false;
-		
-		responseBean.msg = msg;
-		
-		return responseBean;
-	}
-	
-	
-	
 
-	public boolean isSuccess() {
-		return success;
-	}
+    private boolean success;
 
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
+    private Object content;
 
-	public Object getContent() {
-		return content;
-	}
-
-	public void setContent(Object content) {
-		this.content = content;
-	}
+    private Object msg;
 
 
+    /**
+     * @param content 成功的内容
+     * @return
+     * @author Ian.Su
+     */
+    public static ResponseBean successResponse(Object content) {
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.success = true;
+        responseBean.msg = "";
+        responseBean.content = content;
+        return responseBean;
+    }
 
 
-	public Object getMsg() {
-		return msg;
-	}
+    /**
+     * @param msg 失败的内容，比如提示语句
+     * @return
+     * @author Ian.Su
+     */
+    public static ResponseBean errorResponse(Object msg) {
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.success = false;
+        responseBean.msg = msg;
+        return responseBean;
+    }
 
 
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    public Object getMsg() {
+        return msg;
+    }
+
+    public void setMsg(Object msg) {
+        this.msg = msg;
+    }
 
 
-	public void setMsg(Object msg) {
-		this.msg = msg;
-	}
-	
-	
-	
-	
-	
 }
