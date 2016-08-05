aa408dc794c8d4f4a79cdfb227d1bafe8fd45a5b
xumin <xumin@bbdservice.com>
2016-08-05 19:29:38 +0800

    add create_by ,create_date

--- wtyh-domain/src/main/java/com/bbd/wtyh/domain/CompanyBuildingDO.java
+++ wtyh-domain/src/main/java/com/bbd/wtyh/domain/CompanyBuildingDO.java
@@ -1,9 +1,19 @@
 package com.bbd.wtyh.domain;
 
 public class CompanyBuildingDO extends BaseDO {
+    private Integer id;
+
     private Integer companyId;
 
     private Integer buildingId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public Integer getCompanyId() {
         return companyId;
