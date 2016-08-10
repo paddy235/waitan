47eaf920daef4b57222d8310daf8f54d7ae1faae
yanwenyuan <yanwenyuan@bbdservice.com>
2016-08-10 20:33:37 +0800

    关联图谱-重构完成

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RelationController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RelationController.java
@@ -18,70 +18,72 @@ import java.util.Map;
 
 /**
  * 关联方动态图谱控制器
- * 
- * @author Barney Lau
  *
+ * @author Barney Lau
  */
 @Controller
 public class RelationController {
-	
-	@Autowired
-	private RegisterUniversalFilterChainImp registerUniversalFilterChainImp;
-	@Autowired
-	private RelationService relationService;
-	@Autowired
-	private SearchAPIandRelatedPartyService searchAPIandRelatedPartyService;
-	
-	@SuppressWarnings("rawtypes")
-	@RequestMapping(value = "/queryDynamicPicData")
-	public @ResponseBody Map<String, List> queryDynamicPicData(HttpServletRequest request) {
-		try {
-			String companyName = request.getParameter("companyName");
-			String dataVersion = request.getParameter("dataVersion");
-			Integer degree = Integer.valueOf(request.getParameter("degree"));
-			if (StringUtils.isNullOrEmpty(dataVersion)) {
-				dataVersion = (String) request.getSession().getAttribute("defaultVersion");
-			}
-			return registerUniversalFilterChainImp.queryRelation(companyName, dataVersion, degree);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	/**
-	 * 
-	 * 动态关联方 目标公司数据基本信息
-	 *            目标公司名称
-	 *            被查询公司名称
-	 * @return
-	 * @throws Exception
-	 */
-	@SuppressWarnings({ "rawtypes" })
-	@RequestMapping("relatedPartyStatistics")
-	public @ResponseBody
-	AjaxVO relatedPartyStatistics(HttpServletRequest request) throws Exception {
-		String origCompanyName = request.getParameter("origCompanyName");
-		String dataVersion = request.getParameter("dataVersion");
-		String tarCompanyName = request.getParameter("tarCompanyName");
-		Integer degree = Integer.valueOf(request.getParameter("degree"));
-		if (StringUtils.isNullOrEmpty(dataVersion)) {
-			dataVersion = (String) request.getSession().getAttribute("defaultVersion");
-		}
-		AjaxVO ajax = new AjaxVO();
-		Map<String, List> map = registerUniversalFilterChainImp.queryRelation(origCompanyName, dataVersion, degree);
-		if (null != map) {
-			CompanyDataStatisticsVO routeList = searchAPIandRelatedPartyService.relatedPartyStatistics(origCompanyName, tarCompanyName, map.get("lineList"));
-			if (null != routeList) {
-				ajax.setSuccess(1);
-				ajax.setObj(routeList);
-			} else {
-				ajax.setSuccess(0);
-			}
-		} else {
-			ajax.setSuccess(0);
-		}
-		return ajax;
-	}
+
+    @Autowired
+    private RegisterUniversalFilterChainImp registerUniversalFilterChainImp;
+    @Autowired
+    private RelationService relationService;
+    @Autowired
+    private SearchAPIandRelatedPartyService searchAPIandRelatedPartyService;
+
+    @SuppressWarnings("rawtypes")
+    @RequestMapping(value = "/queryDynamicPicData")
+    public
+    @ResponseBody
+    Map<String, List> queryDynamicPicData(HttpServletRequest request) {
+        try {
+            String companyName = request.getParameter("companyName");
+            String dataVersion = request.getParameter("dataVersion");
+            Integer degree = Integer.valueOf(request.getParameter("degree"));
+            if (StringUtils.isNullOrEmpty(dataVersion)) {
+                dataVersion = (String) request.getSession().getAttribute("defaultVersion");
+            }
+            return registerUniversalFilterChainImp.queryRelation(companyName, dataVersion, degree);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 动态关联方 目标公司数据基本信息
+     * 目标公司名称
+     * 被查询公司名称
+     *
+     * @return
+     * @throws Exception
+     */
+    @SuppressWarnings({"rawtypes"})
+    @RequestMapping("relatedPartyStatistics")
+    public
+    @ResponseBody
+    AjaxVO relatedPartyStatistics(HttpServletRequest request) throws Exception {
+        String origCompanyName = request.getParameter("origCompanyName");
+        String dataVersion = request.getParameter("dataVersion");
+        String tarCompanyName = request.getParameter("tarCompanyName");
+        Integer degree = Integer.valueOf(request.getParameter("degree"));
+        if (StringUtils.isNullOrEmpty(dataVersion)) {
+            dataVersion = (String) request.getSession().getAttribute("defaultVersion");
+        }
+        AjaxVO ajax = new AjaxVO();
+        Map<String, List> map = registerUniversalFilterChainImp.queryRelation(origCompanyName, dataVersion, degree);
+        if (null != map) {
+            CompanyDataStatisticsVO routeList = searchAPIandRelatedPartyService.relatedPartyStatistics(origCompanyName, tarCompanyName, map.get("lineList"));
+            if (null != routeList) {
+                ajax.setSuccess(1);
+                ajax.setObj(routeList);
+            } else {
+                ajax.setSuccess(0);
+            }
+        } else {
+            ajax.setSuccess(0);
+        }
+        return ajax;
+    }
 
 }
