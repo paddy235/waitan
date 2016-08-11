7bb42b77f8b77a6d74dd885e277ebdf7ea843e57
Barney <liuzhaoqiang@brandbigdata.com>
2016-08-11 10:19:35 +0800

    udpate

---
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/impl/TaskServiceImpl.java
@@ -0,0 +1,32 @@
+package com.bbd.wtyh.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.bbd.wtyh.mapper.SysAnalyzeMapper;
+import com.bbd.wtyh.service.TaskService;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+	
+	private static int baseNo = 30;
+	@Autowired
+	private SysAnalyzeMapper sysAnalyzeMapper;
+	
+	@Scheduled(cron = "0 0/1 * * * ?")
+	@Override
+	public void updateSysAnalyze() {
+		Map<String, Object> params = new HashMap<>();
+		Random random = new Random();
+		params.put("dataCount", baseNo + random.nextInt(9));
+		params.put("bugCount", baseNo + random.nextInt(9));
+		
+		sysAnalyzeMapper.updateSysAnalyze(params);
+	}
+
+}
