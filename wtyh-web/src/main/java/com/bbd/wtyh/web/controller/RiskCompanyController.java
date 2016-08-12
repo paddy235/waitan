4fe5f963a9329a325c81f635e1fadacabd594326
Barney <liuzhaoqiang@brandbigdata.com>
2016-08-12 09:57:10 +0800

    update

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RiskCompanyController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RiskCompanyController.java
@@ -3,8 +3,6 @@ package com.bbd.wtyh.web.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,31 +15,36 @@ import com.bbd.wtyh.web.ResponseBean;
 @Controller
 @RequestMapping(value = "/risk")
 public class RiskCompanyController {
-	
+
 	@Autowired
 	private RiskCompanyService riskCompanyService;
-	
+
 	@RequestMapping(value = "/getScanner")
 	@ResponseBody
-	public ResponseBean getScanner(@RequestParam(required=false) String area, @RequestParam(required=false) String minRegCapital, 
-			@RequestParam(required=false) String maxRegCapital, @RequestParam(required=false) String companyQualification, 
-			@RequestParam(required=false) String minReviewTime, @RequestParam(required=false) String maxReviewTime) {
-		Map<String, Object> params = this.fillMap(area, minRegCapital, maxRegCapital, companyQualification, minReviewTime, maxReviewTime);
+	public ResponseBean getScanner(@RequestParam(required = false) String area,
+			@RequestParam(required = false) String minRegCapital, @RequestParam(required = false) String maxRegCapital,
+			@RequestParam(required = false) String companyQualification,
+			@RequestParam(required = false) String minReviewTime,
+			@RequestParam(required = false) String maxReviewTime) {
+		Map<String, Object> params = this.fillMap(area, minRegCapital, maxRegCapital, companyQualification,
+				minReviewTime, maxReviewTime, null);
 		return ResponseBean.successResponse(riskCompanyService.getScanner(params));
 	}
-	
-	@RequestMapping(value = "/getScannerCount")
+
+	@RequestMapping(value = "/getTop")
 	@ResponseBody
-	public ResponseBean getScannerCount(@RequestParam(required=false) String area, @RequestParam(required=false) String minRegCapital, 
-			@RequestParam(required=false) String maxRegCapital, @RequestParam(required=false) String companyQualification, 
-			@RequestParam(required=false) String minReviewTime, @RequestParam(required=false) String maxReviewTime) {
-		Map<String, Object> params = this.fillMap(area, minRegCapital, maxRegCapital, companyQualification, minReviewTime, maxReviewTime);
-		return ResponseBean.successResponse(riskCompanyService.getScannerCount(params));
+	public ResponseBean getTop(@RequestParam(required = false) String area,
+			@RequestParam(required = false) String minRegCapital, @RequestParam(required = false) String maxRegCapital,
+			@RequestParam(required = false) String companyQualification,
+			@RequestParam(required = false) String minReviewTime, @RequestParam(required = false) String maxReviewTime,
+			@RequestParam(defaultValue = "0") String sortType) {
+		Map<String, Object> params = this.fillMap(area, minRegCapital, maxRegCapital, companyQualification,
+				minReviewTime, maxReviewTime, sortType);
+		return ResponseBean.successResponse(riskCompanyService.getTop(params));
 	}
-	
-	private Map<String, Object> fillMap(String area, String minRegCapital, 
-			String maxRegCapital, String companyQualification, 
-			String minReviewTime, String maxReviewTime) {
+
+	private Map<String, Object> fillMap(String area, String minRegCapital, String maxRegCapital,
+			String companyQualification, String minReviewTime, String maxReviewTime, String sortType) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("area", area);
 		map.put("minRegCapital", minRegCapital);
@@ -49,6 +52,7 @@ public class RiskCompanyController {
 		map.put("companyQualification", companyQualification);
 		map.put("minReviewTime", minReviewTime);
 		map.put("maxReviewTime", maxReviewTime);
+		map.put("sortType", sortType);
 		return map;
 	}
 
