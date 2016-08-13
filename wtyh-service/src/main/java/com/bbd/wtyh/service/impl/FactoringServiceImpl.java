5096b0241781a744acdd64def1d740c3e07a46bf
suyin <suyin@bbdservice.com>
2016-08-13 14:16:04 +0800

    添加商业保理新接口

--- wtyh-service/src/main/java/com/bbd/wtyh/service/impl/FactoringServiceImpl.java
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/impl/FactoringServiceImpl.java
@@ -1,12 +1,14 @@
 package com.bbd.wtyh.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bbd.wtyh.domain.CommercialFactoringStatisticDO;
 import com.bbd.wtyh.domain.CompanyAndRiskDO;
+import com.bbd.wtyh.domain.CompanyCountDO;
 import com.bbd.wtyh.mapper.FactoringMapper;
 import com.bbd.wtyh.service.FactoringService;
 
@@ -31,6 +33,27 @@ public class FactoringServiceImpl implements FactoringService {
 	@Override
 	public List<CompanyAndRiskDO> queryFactoringCompanys() {
 		return facMapper.queryFactoringCompanys();
+	}
+
+
+
+	@Override
+	public List<Map<String,Object>> distribute() {
+		return facMapper.distribute();
+	}
+
+
+
+	@Override
+	public List<Map<String, Object>> geogDistribute() {
+		return facMapper.geogDistribute();
+	}
+
+
+	
+	@Override
+	public List<CompanyCountDO> countCapitalBySeason() {
+		return facMapper.countCapitalBySeason();
 	}
 
 }
