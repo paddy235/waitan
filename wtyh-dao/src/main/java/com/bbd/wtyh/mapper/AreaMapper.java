cf20cf36baa9213128a91c25ac3d762ae8edd7a6
xumin <xumin@bbdservice.com>
2016-08-16 10:27:23 +0800

    add shareholder risk for loan&guarantee company

--- wtyh-dao/src/main/java/com/bbd/wtyh/mapper/AreaMapper.java
+++ wtyh-dao/src/main/java/com/bbd/wtyh/mapper/AreaMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bbd.wtyh.domain.AreaDO;
 import com.bbd.wtyh.domain.NvDO;
+import org.apache.ibatis.annotations.Param;
 
 
 /**
@@ -25,4 +26,10 @@ public interface AreaMapper{
 	* @return List<NvDO>
 	*/
 	List<NvDO> hot();
+
+
+	AreaDO selectByNameAndLevel(@Param("name") String name,@Param("level") Integer level);
+
+	List<AreaDO> selectByParentId(Integer parentId);
+
 }
