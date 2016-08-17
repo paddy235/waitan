63c9189ae91a9f63b7532fcef25360a6f4dfaf39
xuan <zhouxuan@bbdservice.com>
2016-08-17 16:42:20 +0800

    add real time monitor business api

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RealTimeMonitorController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RealTimeMonitorController.java
@@ -1,5 +1,6 @@
 package com.bbd.wtyh.web.controller;
 
+import com.bbd.wtyh.domain.CapitalAmountDO;
 import com.bbd.wtyh.domain.CommercialFactoringStatisticDO;
 import com.bbd.wtyh.domain.CompanyDO;
 import com.bbd.wtyh.domain.LoanBalanceDO;
@@ -13,6 +14,7 @@ import com.bbd.wtyh.web.ResponseBean;
 import com.bbd.wtyh.web.XAxisSeriesBarLineBean;
 import com.bbd.wtyh.web.XAxisSeriesLinesBean;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -55,6 +58,10 @@ public class RealTimeMonitorController {
     private FinanceLeaseController financeLeaseController;
     @Autowired
     private ExchangeCompanyController exchangeCompanyController;
+    @Autowired
+    private PrivateFundService privateFundService;
+    @Autowired
+    private ExchangeCompanyService exchangeCompanyService;
 
     /**
      * 光谱分析 - 只做标识，前端区分
@@ -151,7 +158,17 @@ public class RealTimeMonitorController {
         //小贷
         ResponseBean loanResponseBean = loanController.balance();
         //私募
-        ResponseBean privateFundResponseBean = privateFundController.capitalAmount();
+        List<CapitalAmountDO> capitalAmountList = privateFundService.capitalAmount();
+        @SuppressWarnings("unchecked")
+        XAxisSeriesLinesBean<String,String> privateDTO = new XAxisSeriesLinesBean<>(
+                new ArrayList<String>(),
+                new ArrayList<String>());
+
+        for (CapitalAmountDO capitalAmountDO : capitalAmountList) {
+            privateDTO.getxAxis().add(privateFundService.getTypeById(capitalAmountDO.getTypeId()).getTypeName());
+            privateDTO.getSeries()[0].add(capitalAmountDO.getManagedCapitalAmount().toString());
+            privateDTO.getSeries()[1].add(capitalAmountDO.getPublishCompanyNumber().toString());
+        }
         //p2p
         XAxisSeriesBarLineBean<Integer,String> pToPMonitorResponseBean = new XAxisSeriesBarLineBean<>();
         try {
@@ -163,7 +180,13 @@ public class RealTimeMonitorController {
         //融资租赁
         ResponseBean financeLeaseResponseBean = financeLeaseController.leaseCompanyStatistic();
         //交易场所分类
-        ResponseBean exchangeCompanyResponseBean = exchangeCompanyController.exchangeCompanyCategory();
+        List<Map> exchangeCompanyData = exchangeCompanyService.exchangeCompanyCategory();
+        HistogramBean<String,String> exchangeCompanyBean = new HistogramBean<>();
+
+        for (Map map : exchangeCompanyData) {
+            exchangeCompanyBean.getxAxis().add(map.keySet().iterator().next().toString());
+            exchangeCompanyBean.getseries().add(map.values().iterator().next().toString());
+        }
         //众筹平台
         ResponseBean crowdFundingResponseBean = crowdFundingController.newlyProject();
         //典当
@@ -184,12 +207,12 @@ public class RealTimeMonitorController {
 //        }
         //预付卡
         ResponseBean prepaidCompanyResponseBean = prepaidCompanyController.amount();
-        Map result = new HashedMap();
+        Map result = new LinkedHashMap();
         result.put("loan", loanResponseBean.getContent());
-        result.put("private", privateFundResponseBean.getContent());
+        result.put("private", privateDTO);
         result.put("p2p", pToPMonitorResponseBean);
         result.put("finance", financeLeaseResponseBean.getContent());
-        result.put("exchange", exchangeCompanyResponseBean.getContent());
+        result.put("exchange", exchangeCompanyBean);
         result.put("crowd", crowdFundingResponseBean.getContent());
         result.put("mortgage", mortgageResponseBean.getContent());
 //        result.put("factoring", dto);
