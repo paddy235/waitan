4a8fc1c635d45b4c49c71745b08c21233af569fd
martin <wangchenge@bbdservice.com>
2016-08-18 09:33:40 +0800

    rename requestmapping URL

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RealTimeMonitorController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/RealTimeMonitorController.java
@@ -32,7 +32,7 @@ import java.util.Map;
  * @since 2016.08.13
  */
 @Controller
-@RequestMapping("/realTimeMonitorController")
+@RequestMapping("/realTimeMonitor")
 public class RealTimeMonitorController {
     @Autowired
     private RealTimeMonitorService realTimeMonitorService;
