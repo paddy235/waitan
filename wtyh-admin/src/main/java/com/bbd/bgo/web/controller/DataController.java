28f3cd9787e801cd52ec2f3995a6cd4115c3bfc2
suyin <suyin@bbdservice.com>
2016-08-18 15:18:49 +0800

    test

--- wtyh-admin/src/main/java/com/bbd/bgo/web/controller/DataController.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/web/controller/DataController.java
@@ -51,7 +51,9 @@ public class DataController {
 	 */
 	@RequestMapping("/showTables")
 	public String showTables() {
-		
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
 		return "geo/showTables";
 	}
 	
