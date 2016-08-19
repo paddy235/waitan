bcf207826e5a0f88a3bb2c676601fdc84543f81b
xuan <zhouxuan@bbdservice.com>
2016-08-19 09:44:21 +0800

    modify related company api

---
+++ wtyh-domain/src/main/java/com/bbd/wtyh/domain/vo/RelationDataIndexVO.java
@@ -0,0 +1,142 @@
+package com.bbd.wtyh.domain.vo;
+
+import java.util.List;
+
+public class RelationDataIndexVO {
+
+	private String companyName; // 公司名称
+	private String dataVersion; // 数据版本
+	private String riskDate;    // 风险时间
+	private int relationPersonNum; //关联自然人
+	private int companyPersonNum;  //关联法人
+	private int childCompanyNum;   //子公司数量
+	private String childCompanyDetail; //子公司明细
+	private int onePersonNum;      //一度自然人
+	private String onePersonDetail;//一度自然人明细
+	private int oneCompanyNum;	   //一度法人
+	private String oneCompanyDetail;//一度法人明细
+	private int twoPersonNum;      //二度自然人
+	private String twoPersonDetail;//二度自然人明细
+	private int twoCompanyNum;     //二度法人
+	private String twoCompanyDetail;//二度法人明细
+	private int threePersonNum;    //三度自然人
+	private String threePersonDetail;//三度自然人明细
+	private int threeCompanyNum;   //三度法人
+	private String threeCompanyDetail; //三度法人明细
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getDataVersion() {
+		return dataVersion;
+	}
+	public void setDataVersion(String dataVersion) {
+		this.dataVersion = dataVersion;
+	}
+	public String getRiskDate() {
+		return riskDate;
+	}
+	public void setRiskDate(String riskDate) {
+		this.riskDate = riskDate;
+	}
+	public int getRelationPersonNum() {
+		return relationPersonNum;
+	}
+	public void setRelationPersonNum(int relationPersonNum) {
+		this.relationPersonNum = relationPersonNum;
+	}
+	public int getCompanyPersonNum() {
+		return companyPersonNum;
+	}
+	public void setCompanyPersonNum(int companyPersonNum) {
+		this.companyPersonNum = companyPersonNum;
+	}
+	public int getChildCompanyNum() {
+		return childCompanyNum;
+	}
+	public void setChildCompanyNum(int childCompanyNum) {
+		this.childCompanyNum = childCompanyNum;
+	}
+	public String getChildCompanyDetail() {
+		return childCompanyDetail;
+	}
+	public void setChildCompanyDetail(String childCompanyDetail) {
+		this.childCompanyDetail = childCompanyDetail;
+	}
+	public int getOnePersonNum() {
+		return onePersonNum;
+	}
+	public void setOnePersonNum(int onePersonNum) {
+		this.onePersonNum = onePersonNum;
+	}
+	public String getOnePersonDetail() {
+		return onePersonDetail;
+	}
+	public void setOnePersonDetail(String onePersonDetail) {
+		this.onePersonDetail = onePersonDetail;
+	}
+	public int getOneCompanyNum() {
+		return oneCompanyNum;
+	}
+	public void setOneCompanyNum(int oneCompanyNum) {
+		this.oneCompanyNum = oneCompanyNum;
+	}
+	public String getOneCompanyDetail() {
+		return oneCompanyDetail;
+	}
+	public void setOneCompanyDetail(String oneCompanyDetail) {
+		this.oneCompanyDetail = oneCompanyDetail;
+	}
+	public int getTwoPersonNum() {
+		return twoPersonNum;
+	}
+	public void setTwoPersonNum(int twoPersonNum) {
+		this.twoPersonNum = twoPersonNum;
+	}
+	public String getTwoPersonDetail() {
+		return twoPersonDetail;
+	}
+	public void setTwoPersonDetail(String twoPersonDetail) {
+		this.twoPersonDetail = twoPersonDetail;
+	}
+	public int getTwoCompanyNum() {
+		return twoCompanyNum;
+	}
+	public void setTwoCompanyNum(int twoCompanyNum) {
+		this.twoCompanyNum = twoCompanyNum;
+	}
+	public String getTwoCompanyDetail() {
+		return twoCompanyDetail;
+	}
+	public void setTwoCompanyDetail(String twoCompanyDetail) {
+		this.twoCompanyDetail = twoCompanyDetail;
+	}
+	public int getThreePersonNum() {
+		return threePersonNum;
+	}
+	public void setThreePersonNum(int threePersonNum) {
+		this.threePersonNum = threePersonNum;
+	}
+	public String getThreePersonDetail() {
+		return threePersonDetail;
+	}
+	public void setThreePersonDetail(String threePersonDetail) {
+		this.threePersonDetail = threePersonDetail;
+	}
+	public int getThreeCompanyNum() {
+		return threeCompanyNum;
+	}
+	public void setThreeCompanyNum(int threeCompanyNum) {
+		this.threeCompanyNum = threeCompanyNum;
+	}
+	public String getThreeCompanyDetail() {
+		return threeCompanyDetail;
+	}
+	public void setThreeCompanyDetail(String threeCompanyDetail) {
+		this.threeCompanyDetail = threeCompanyDetail;
+	}
+	
+	
+}
---
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/RelationDataService.java
@@ -0,0 +1,36 @@
+package com.bbd.wtyh.service;
+
+
+import com.bbd.wtyh.domain.vo.DynamicRiskVO;
+import com.bbd.wtyh.domain.vo.RelationDataVO;
+
+import java.util.List;
+
+/**
+*
+* @author
+* @since 2016.08.13
+*/
+public interface RelationDataService {
+
+	public RelationDataVO compareRelationData(String companyName, String areaCode, String currentMonth, String compareMonth);
+
+	/**
+	 *
+	 * 比较两个月动态风险指标结果
+	 * @param companyName
+	 * @param currentMonth
+	 * @param compareMonth
+	 * @return
+	 */
+	public DynamicRiskVO compareDynamicRisk(String companyName, String areaCode, String currentMonth, String compareMonth);
+
+	/**
+	 *
+	 * @param companyName
+	 * @param areaCode
+     * @return
+     */
+	public List<String> queryDateVersion(String companyName, String areaCode);
+
+}
