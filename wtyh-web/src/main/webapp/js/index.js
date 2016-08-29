495364570b622beab69ab8a92ddc5a7bf45056ca
xuyao <xuyao@bbdservice.com>
2016-08-29 17:58:45 +0800

    add

--- wtyh-web/src/main/webapp/js/index.js
+++ wtyh-web/src/main/webapp/js/index.js
@@ -49662,7 +49662,7 @@
 	                    normal: {
 	                        areaColor: 'none',
 	                        borderColor: '#0cbaef',
-	                        borderWidth: 2
+	                        borderWidth: 0.4
 	                    },
 	                    emphasis: {
 	                        areaColor: '#e6e6e6'
@@ -49734,7 +49734,7 @@
 	                },
 	                itemStyle: {
 	                    normal: {
-	                        color: "#ddb926"
+	                        color: "#f19149"
 	                    }
 	                },
 	                data: SHData.map(function (dataItem) {
@@ -49766,7 +49766,7 @@
 	                },
 	                itemStyle: {
 	                    normal: {
-	                        color: "red"
+	                        color: "#d81111"
 	                    }
 	                },
 	                data: [{
@@ -49794,7 +49794,7 @@
 	                    normal: {
 	                        areaColor: 'none',
 	                        borderColor: '#0cbaef',
-	                        borderWidth: 2
+	                        borderWidth: 0.4
 	                    },
 	                    emphasis: {
 	                        areaColor: '#e6e6e6'
@@ -50140,124 +50140,124 @@
 	    },
 
 	    changeNum: function changeNum(data) {
-	        $.fn.numberAnimate = function (setting) {
-	            var defaults = {
-	                speed: 1000, //动画速度
-	                num: "", //初始化值
-	                iniAnimate: true, //是否要初始化动画效果
-	                symbol: '', //默认的分割符号，千，万，千万
-	                dot: 0 //保留几位小数点
-	            };
-	            //如果setting为空，就取default的值
-	            var setting = $.extend(defaults, setting);
-	            //如果对象有多个，提示出错
-	            if ($(this).length > 1) {
-	                alert("just only one obj!");
-	                return;
-	            }
-	            //如果未设置初始化值。提示出错
-	            if (setting.num == "") {
-	                alert("must set a num!");
-	                return;
-	            }
-	            var nHtml = '<div class="mt-number-animate-dom" data-num="{{num}}">\
-	                        <span class="mt-number-animate-span">0</span>\
-	                        <span class="mt-number-animate-span">1</span>\
-	                        <span class="mt-number-animate-span">2</span>\
-	                        <span class="mt-number-animate-span">3</span>\
-	                        <span class="mt-number-animate-span">4</span>\
-	                        <span class="mt-number-animate-span">5</span>\
-	                        <span class="mt-number-animate-span">6</span>\
-	                        <span class="mt-number-animate-span">7</span>\
-	                        <span class="mt-number-animate-span">8</span>\
-	                        <span class="mt-number-animate-span">9</span>\
-	                        <span class="mt-number-animate-span">.</span>\
-	                    </div>';
-	            //数字处理
-	            var numToArr = function numToArr(num) {
-	                num = parseFloat(num).toFixed(setting.dot);
-	                if (typeof num == 'number') {
-	                    var arrStr = num.toString().split("");
-	                } else {
-	                    var arrStr = num.split("");
-	                }
-	                //console.log(arrStr);
-	                return arrStr;
-	            };
-	            //设置DOM symbol:分割符号
-	            var setNumDom = function setNumDom(arrStr) {
-	                var shtml = '<div class="mt-number-animate">';
-	                for (var i = 0, len = arrStr.length; i < len; i++) {
-	                    if (i != 0 && (len - i) % 3 == 0 && setting.symbol != "" && arrStr[i] != ".") {
-	                        shtml += '<div class="mt-number-animate-dot">' + setting.symbol + '</div>' + nHtml.replace("{{num}}", arrStr[i]);
-	                    } else {
-	                        shtml += nHtml.replace("{{num}}", arrStr[i]);
-	                    }
-	                }
-	                shtml += '</div>';
-	                return shtml;
-	            };
-	            //执行动画
-	            var runAnimate = function runAnimate($parent) {
-	                $parent.find(".mt-number-animate-dom").each(function () {
-	                    var num = $(this).attr("data-num");
-	                    num = num == "." ? 10 : num;
-	                    var spanHei = $(this).height() / 11; //11为元素个数
-	                    var thisTop = -num * spanHei + "px";
-	                    var transform = $(this).css("transform");
-	                    if (transform == 'none' || -num * spanHei != parseInt(transform.split(",")[5], 10)) {
-	                        if (setting.iniAnimate) {
-	                            //HTML5不支持
-	                            if (!window.applicationCache) {
-	                                $(this).animate({
-	                                    top: thisTop
-	                                }, setting.speed);
-	                            } else {
-	                                $(this).css({
-	                                    'transform': 'translateY(' + thisTop + ')',
-	                                    '-ms-transform': 'translateY(' + thisTop + ')', /* IE 9 */
-	                                    '-moz-transform': 'translateY(' + thisTop + ')', /* Firefox */
-	                                    '-webkit-transform': 'translateY(' + thisTop + ')', /* Safari 和 Chrome */
-	                                    '-o-transform': 'translateY(' + thisTop + ')',
-	                                    '-ms-transition': setting.speed / 1000 + 's',
-	                                    '-moz-transition': setting.speed / 1000 + 's',
-	                                    '-webkit-transition': setting.speed / 1000 + 's',
-	                                    '-o-transition': setting.speed / 1000 + 's',
-	                                    'transition': setting.speed / 1000 + 's'
-	                                });
-	                            }
-	                        } else {
-	                            setting.iniAnimate = true;
-	                            $(this).css({
-	                                top: thisTop
-	                            });
-	                        }
-	                    }
-	                });
-	            };
-	            //初始化
-	            var init = function init($parent) {
-	                //初始化
-	                $parent.html(setNumDom(numToArr(setting.num)));
-	                runAnimate($parent);
-	            };
-	            //重置参数
-	            this.resetData = function (num) {
-	                var newArr = numToArr(num);
-	                var $dom = $(this).find(".mt-number-animate-dom");
-	                if ($dom.length < newArr.length) {
-	                    $(this).html(setNumDom(numToArr(num)));
-	                } else {
-	                    $dom.each(function (index, el) {
-	                        $(this).attr("data-num", newArr[index]);
-	                    });
-	                }
-	                runAnimate($(this));
-	            };
-	            //init
-	            init($(this));
-	            return this;
-	        };
+	        // $.fn.numberAnimate = function(setting) {
+	        //     var defaults = {
+	        //         speed : 1000,//动画速度
+	        //         num : "", //初始化值
+	        //         iniAnimate : true, //是否要初始化动画效果
+	        //         symbol : '',//默认的分割符号，千，万，千万
+	        //         dot : 0 //保留几位小数点
+	        //     }
+	        //     //如果setting为空，就取default的值
+	        //     var setting = $.extend(defaults, setting);
+	        //     //如果对象有多个，提示出错
+	        //     if($(this).length > 1){
+	        //         alert("just only one obj!");
+	        //         return;
+	        //     }
+	        //     //如果未设置初始化值。提示出错
+	        //     if(setting.num == ""){
+	        //         alert("must set a num!");
+	        //         return;
+	        //     }
+	        //     var nHtml = '<div class="mt-number-animate-dom" data-num="{{num}}">\
+	        //                     <span class="mt-number-animate-span">0</span>\
+	        //                     <span class="mt-number-animate-span">1</span>\
+	        //                     <span class="mt-number-animate-span">2</span>\
+	        //                     <span class="mt-number-animate-span">3</span>\
+	        //                     <span class="mt-number-animate-span">4</span>\
+	        //                     <span class="mt-number-animate-span">5</span>\
+	        //                     <span class="mt-number-animate-span">6</span>\
+	        //                     <span class="mt-number-animate-span">7</span>\
+	        //                     <span class="mt-number-animate-span">8</span>\
+	        //                     <span class="mt-number-animate-span">9</span>\
+	        //                     <span class="mt-number-animate-span">.</span>\
+	        //                 </div>';
+	        //     //数字处理
+	        //     var numToArr = function(num){
+	        //         num = parseFloat(num).toFixed(setting.dot);
+	        //         if(typeof(num) == 'number'){
+	        //             var arrStr = num.toString().split("");    
+	        //         }else{
+	        //             var arrStr = num.split("");
+	        //         }
+	        //         //console.log(arrStr);
+	        //         return arrStr;
+	        //     }
+	        //     //设置DOM symbol:分割符号
+	        //     var setNumDom = function(arrStr){
+	        //         var shtml = '<div class="mt-number-animate">';
+	        //         for(var i=0,len=arrStr.length; i<len; i++){
+	        //             if(i != 0 && (len-i)%3 == 0 && setting.symbol != "" && arrStr[i]!="."){
+	        //                 shtml += '<div class="mt-number-animate-dot">'+setting.symbol+'</div>'+nHtml.replace("{{num}}",arrStr[i]);
+	        //             }else{
+	        //                 shtml += nHtml.replace("{{num}}",arrStr[i]);
+	        //             }
+	        //         }
+	        //         shtml += '</div>';
+	        //         return shtml;
+	        //     }
+	        //     //执行动画
+	        //     var runAnimate = function($parent){
+	        //         $parent.find(".mt-number-animate-dom").each(function() {
+	        //             var num = $(this).attr("data-num");
+	        //             num = (num=="."?10:num);
+	        //             var spanHei = $(this).height()/11; //11为元素个数
+	        //             var thisTop = -num*spanHei+"px";
+	        //             var transform = $(this).css("transform");
+	        //             if(transform=='none' || -num*spanHei != parseInt(transform.split(",")[5], 10)){
+	        //                 if(setting.iniAnimate){
+	        //                     //HTML5不支持
+	        //                     if(!window.applicationCache){
+	        //                         $(this).animate({
+	        //                             top : thisTop
+	        //                         }, setting.speed);
+	        //                     }else{
+	        //                        $(this).css({
+	        //                             'transform':'translateY('+thisTop+')',
+	        //                             '-ms-transform':'translateY('+thisTop+')',     /* IE 9 */
+	        //                             '-moz-transform':'translateY('+thisTop+')',    /* Firefox */
+	        //                             '-webkit-transform':'translateY('+thisTop+')', /* Safari 和 Chrome */
+	        //                             '-o-transform':'translateY('+thisTop+')',
+	        //                             '-ms-transition':setting.speed/1000+'s',
+	        //                             '-moz-transition':setting.speed/1000+'s',
+	        //                             '-webkit-transition':setting.speed/1000+'s',
+	        //                             '-o-transition':setting.speed/1000+'s',
+	        //                             'transition':setting.speed/1000+'s'
+	        //                         }); 
+	        //                     }
+	        //                 }else{
+	        //                     setting.iniAnimate = true;
+	        //                     $(this).css({
+	        //                         top : thisTop
+	        //                     });
+	        //                 }
+	        //             }
+	        //         });
+	        //     }
+	        //     //初始化
+	        //     var init = function($parent){
+	        //         //初始化
+	        //         $parent.html(setNumDom(numToArr(setting.num)));
+	        //         runAnimate($parent);
+	        //     };
+	        //     //重置参数
+	        //     this.resetData = function(num){
+	        //         var newArr = numToArr(num);
+	        //         var $dom = $(this).find(".mt-number-animate-dom");
+	        //         if($dom.length < newArr.length){
+	        //             $(this).html(setNumDom(numToArr(num)));
+	        //         }else{
+	        //             $dom.each(function(index, el) {
+	        //                 $(this).attr("data-num",newArr[index]);
+	        //             });
+	        //         }
+	        //         runAnimate($(this));
+	        //     }
+	        //     //init
+	        //     init($(this));
+	        //     return this;
+	        // }
 	        //数字滚动插件
 	        var numRunA = $(".homeScrollA").numberAnimate({ num: data.dataCount, speed: 2000, symbol: "," });
 	        var numRunB = $(".homeScrollB").numberAnimate({ num: data.companyCount, speed: 2000, symbol: "," });
@@ -60767,7 +60767,7 @@
 	            series: [{
 	                name: 'Punch Card',
 	                type: 'scatter',
-	                symbolSize: function symbolSize(val) {
+	                symbolSize: param.symbolSize || function (val) {
 	                    if (val[1] < 1000) {
 	                        return val[1] / 10;
 	                    } else if (val[1] > 1000 && val[1] < 10000) {
@@ -84718,15 +84718,7 @@
 	                        },
 	                        gridTop: "25%",
 	                        legend: [],
-	                        symbolSize: function symbolSize(val) {
-	                            if (val[3] < 1000) {
-	                                return val[3] / 10;
-	                            } else if (val[3] > 1000 && val[3] < 10000) {
-	                                return val[3] / 400;
-	                            } else {
-	                                return val[3] / 400;
-	                            }
-	                        },
+
 	                        xAxis: nineDate.content.loan.xAxis,
 	                        //yAxis:paramOneyAxis,
 	                        data: paramOneData,
@@ -84883,6 +84875,7 @@
 	                    var paramSevenData = [];
 	                    nineDate.content.mortgage.series[0].map(function (item, index) {
 	                        var x = nineDate.content.mortgage.xAxis.reverse()[index];
+	                        var item = parseFloat(item / 10000).toFixed(2);
 	                        var xx = [x, item];
 	                        paramSevenData.push(xx);
 	                    });
@@ -84905,9 +84898,9 @@
 	                        YnameGap: 5,
 	                        legend: [],
 	                        tooltip: function tooltip(data) {
-	                            return data.data[0] + "<br/>典当总额：" + parseFloat(data.data[1] / 10000).toFixed(2) + "亿元<br/>业务笔数：" + data.data[2];
+	                            return data.data[0] + "<br/>典当总额：" + data.data[1] + "亿元<br/>业务笔数：" + data.data[2];
 	                        },
-	                        xAxis: nineDate.content.mortgage.xAxis.reverse(),
+	                        xAxis: nineDate.content.mortgage.xAxis,
 	                        // yAxis:b,
 	                        data: paramSevenData,
 	                        symbolSize: function symbolSize() {
