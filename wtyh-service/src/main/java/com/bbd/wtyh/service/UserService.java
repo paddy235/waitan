8814a6640edea6a2b9b841ad334b7406506c7ff4
xumin <xumin@bbdservice.com>
2016-11-22 17:20:10 +0800

    新增注释

--- wtyh-service/src/main/java/com/bbd/wtyh/service/UserService.java
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/UserService.java
@@ -16,11 +16,15 @@ public interface UserService {
     String changePassword(String loginName, String oldPassword, String newPassword);
 
     interface ResultCode {
-
+        //账号不存在
         String ACCOUNT_NOT_EXIT          = "account.not.exist";
+        //账户名或密码错误
         String PASSWORD_ERROR            = "password.error";
+        //新密码不能为历史密码
         String PASSWORD_HISTORY_CONTAINS = "password.history.contains";
+        //密码修改成功
         String PASSWORD_CHANGE_SUCCESS   = "password.change.success";
+        //密码需要修改
         String PASSWORD_NEED_CHANGE      = "password.need.change";
 
     }
