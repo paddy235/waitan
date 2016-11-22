bf70764f5aba2957d1ca21b7bda3021451091ca5
xumin <xumin@bbdservice.com>
2016-11-22 17:10:14 +0800

    实现用户修改密码

--- wtyh-service/src/main/java/com/bbd/wtyh/service/UserService.java
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/UserService.java
@@ -1,12 +1,28 @@
 package com.bbd.wtyh.service;
 
+import java.util.Date;
 
 /**
-* 获取用户信息
-* @author Ian.Su
-* @since 2016年8月15日 下午6:23:04
-*/
+ * 获取用户信息
+ * @author Ian.Su
+ * @since 2016年8月15日 下午6:23:04
+ */
 public interface UserService {
 
-	public String getPassword(String loginName);
+    String getPassword(String loginName);
+
+    Date lastChangePasswordDate(String loginName);
+
+    String changePassword(String loginName, String oldPassword, String newPassword);
+
+    interface ResultCode {
+
+        String ACCOUNT_NOT_EXIT          = "account.not.exist";
+        String PASSWORD_ERROR            = "password.error";
+        String PASSWORD_HISTORY_CONTAINS = "password.history.contains";
+        String PASSWORD_CHANGE_SUCCESS   = "password.change.success";
+        String PASSWORD_NEED_CHANGE      = "password.need.change";
+
+    }
+
 }
