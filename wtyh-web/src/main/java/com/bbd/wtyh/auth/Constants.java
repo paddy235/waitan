8cdd4be13e6a79bc3f3bd0d12822c2885bfeb000
sunliming <sunliming@bbdservice.com>
2017-03-04 17:01:20 +0800

    业务平台登录，接口鉴权

---
+++ wtyh-web/src/main/java/com/bbd/wtyh/auth/Constants.java
@@ -0,0 +1,51 @@
+package com.bbd.wtyh.auth;
+
+/**
+ * Created by Administrator on 2017/3/2 0002.
+ */
+public class Constants {
+    //加密salt
+    public static final String SALT="123456";//
+    //状态值
+    public static final String ACTIVE = "A";//激活
+
+    public static final String FREEZE="F";//冻结
+
+    public static final String FRONT="F";//前台角色
+
+    public static final String BACK="B";//后台角色
+
+    //权限列表
+    public static final String F_HYJCPT = "F_HYJCPT";//行业监测平台
+
+    public static final String F_HYJCPT_WLJD = "F_HYJCPT_WLJD";//网络借贷
+
+    public static final String F_HYJCPT_XXLC = "F_HYJCPT_XXLC";//线下理财
+
+    public static final String F_HYJCPT_XEDK = "F_HYJCPT_XEDK";//小额贷款
+
+    public static final String F_HYJCPT_RZDB = "F_HYJCPT_RZDB";//融资担保
+
+    public static final String F_HYJCPT_SMJJ = "F_HYJCPT_SMJJ";//私募基金
+
+    public static final String F_HYJCPT_JYCS = "F_HYJCPT_JYCS";//交易场所
+
+    public static final String F_HYJCPT_ZC = "F_HYJCPT_ZC";//众筹
+
+    public static final String F_HYJCPT_DD = "F_HYJCPT_DD";//典当
+
+    public static final String F_HYJCPT_RZZP = "F_HYJCPT_RZZP";//融资租赁
+
+    public static final String F_HYJCPT_SYBL = "F_HYJCPT_SYBL";//商业保理
+
+    public static final String F_HYJCPT_YFK = "F_HYJCPT_YFK";//预付卡
+
+    public static final String F_YQJCPT = "F_YQJCPT";//园区监测平台
+
+    public static final String F_QYQXXXCXPT = "F_QYQXXXCXPT";//企业全息信息查询平台
+
+    public static final String F_SSJCPT = "F_SSJCPT";//实时监测平台
+
+    public static final String B_HTGLQX = "B_HTGLQX";//后台管理权限
+
+}
--- wtyh-web/src/main/java/com/bbd/wtyh/sys/controller/LoginController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/sys/controller/LoginController.java
@@ -1,12 +1,21 @@
 package com.bbd.wtyh.sys.controller;
 
 import com.bbd.wtyh.common.Constants;
+import com.bbd.wtyh.service.RoleResourceService;
+import com.bbd.wtyh.service.UserInfoService;
 import com.bbd.wtyh.service.UserService;
 import com.bbd.wtyh.web.ResponseBean;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ByteSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +25,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Ian.Su
@@ -23,7 +34,11 @@ import java.util.Date;
  */
 @Controller
 public class LoginController {
-
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private RoleResourceService roleResourceService;
+    @Autowired
+    private UserInfoService userInfoService;
     @Autowired
     private UserService userService;
 
@@ -34,7 +49,7 @@ public class LoginController {
         //		String adminpwd =  new SimpleHash("md5", "bbd54321" ,ByteSource.Util.bytes("123456"),2).toHex();
         //		System.out.println(adminpwd);
 
-        //数据库的密码是bbd54321
+      /*  //数据库的密码是bbd54321
         String pwd = userService.getPassword(name);
         if (StringUtils.isEmpty(pwd)) {
             return ResponseBean.successResponse(false);
@@ -54,7 +69,36 @@ public class LoginController {
             responseBean.setMsg(UserService.ResultCode.PASSWORD_NEED_CHANGE);
         }
 
-        return responseBean;
+        return responseBean;*/
+
+        Map map=null;
+        UsernamePasswordToken token = new UsernamePasswordToken(name, password);
+        token.setRememberMe(true);
+        //获取当前的Subject
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            currentUser.login(token);
+            logger.info(name+"身份验证通过,登录业务系统!");
+            map=new HashedMap();
+            Set set=roleResourceService.queryResourceCodeByLoginName(name);
+            Session session=currentUser.getSession();
+            session.setAttribute("resource",set);//权限列表
+            session.setAttribute(Constants.SESSION.loginName, name);//登录用户名
+            session.setAttribute("area","等功杰提供接口");//用户所属地区编号
+
+            map.put("resource",set);//给前端权限列表
+            map.put(Constants.SESSION.loginName, name);//给前端录用户名
+            map.put("area","等功杰提供接口");//给前端权限地区编号
+
+        }catch(Exception e){
+
+            //通过处理Shiro的运行时AuthenticationException就可以控制用户登录失败或密码错误时的情景
+            e.printStackTrace();
+            //request.setAttribute("message_login", "用户名或密码不正确");
+            return ResponseBean.errorResponse("用户名或密码不正确");
+        }
+
+        return ResponseBean.successResponse(map);
 
     }
 
@@ -69,9 +113,15 @@ public class LoginController {
     @ResponseBody
     public Object logout(HttpServletRequest request) {
 
-        HttpSession se = request.getSession(false);
+        /*HttpSession se = request.getSession(false);
         if (se != null)
             se.invalidate();
+
+        return ResponseBean.successResponse(true);*/
+
+        Subject currentUser = SecurityUtils.getSubject();
+        logger.info(currentUser.getPrincipal()+"登出业务系统!");
+        currentUser.logout();
 
         return ResponseBean.successResponse(true);
     }
