81b374766256e1c2db3240a9596a21384c2dd1bd
sunliming <sunliming@bbdservice.com>
2017-03-04 13:32:11 +0800

    角色权限控制

---
+++ wtyh-admin/src/main/java/com/bbd/bgo/auth/UserRealm.java
@@ -0,0 +1,66 @@
+package com.bbd.bgo.auth;
+
+
+import com.bbd.wtyh.service.RoleResourceService;
+import com.bbd.wtyh.service.UserService;
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2017/2/27 0027.
+ */
+public class UserRealm extends AuthorizingRealm {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RoleResourceService roleResourceService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(
+            PrincipalCollection principalCollection) {
+        System.out.println("角色权限检查");
+        //获取当前登入的用户名
+        String userName=(String)principalCollection.getPrimaryPrincipal();
+        if(!StringUtils.isEmpty(userName)){
+            SimpleAuthorizationInfo authorizationInfo=new SimpleAuthorizationInfo();
+            //根据此用户名查询是否拥有此角色 返回的是一个字符串集合
+            //authorizationInfo.setRoles(roleResourceService.queryRoleCodeByLoginName(userName));
+            //根据用户名查询是否拥有此权限 返回的是一个字符串集合
+            authorizationInfo.setStringPermissions(roleResourceService.queryResourceCodeByLoginName(userName));
+
+            return authorizationInfo;
+        }
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(
+            AuthenticationToken authenticationToken) throws AuthenticationException {
+        // TODO Auto-generated method stub
+        System.out.println("身份验证");
+        UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken;
+        String username=token.getUsername();
+        char[] password=token.getPassword();
+        String pwd = userService.getPassword(username);
+        if (StringUtils.isEmpty(pwd)) {
+            throw new UnknownAccountException(); //如果用户名错误;
+        }
+
+        String dataPwd = new SimpleHash("md5", password, ByteSource.Util.bytes(Constants.SALT), 2).toHex();
+
+        if (!dataPwd.equals(pwd)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
--- wtyh-admin/src/main/java/com/bbd/bgo/web/controller/LoginController.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/web/controller/LoginController.java
@@ -1,10 +1,19 @@
 package com.bbd.bgo.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.bbd.wtyh.service.RoleResourceService;
 import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.web.servlet.ShiroHttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +24,8 @@ import com.bbd.wtyh.common.Constants;
 import com.bbd.wtyh.service.UserService;
 import com.bbd.wtyh.web.ResponseBean;
 
+import java.util.Set;
+
 /**
 * 
 * @author Ian.Su
@@ -22,61 +33,50 @@ import com.bbd.wtyh.web.ResponseBean;
 */
 @Controller
 public class LoginController {
-	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private RoleResourceService roleResourceService;
 	@Autowired
 	private UserService userSer;
 
 	@RequestMapping("/login")
 	@ResponseBody
 	public Object login(@RequestParam String name,@RequestParam String password,HttpServletRequest request){
-		//数据库的密码是bbd123456
-		String pwd = userSer.getPassword(name);
-		if( StringUtils.isEmpty(pwd) ){
+
+		UsernamePasswordToken token = new UsernamePasswordToken(name, password);
+		token.setRememberMe(true);
+		//获取当前的Subject
+		Subject currentUser = SecurityUtils.getSubject();
+		try {
+			currentUser.login(token);
+			logger.info(name+"身份验证通过,登录后台系统!");
+			Set set=roleResourceService.queryResourceCodeByLoginName(name);
+			Session session=currentUser.getSession();
+			session.setAttribute("resource",set);//权限列表
+			session.setAttribute(Constants.SESSION.loginName, name);//登录用户名
+			session.setAttribute("area","");//用户所属地区编号
+
+
+		}catch(Exception e){
+
+			//通过处理Shiro的运行时AuthenticationException就可以控制用户登录失败或密码错误时的情景
+			e.printStackTrace();
+			//request.setAttribute("message_login", "用户名或密码不正确");
 			return ResponseBean.errorResponse("false");
 		}
-		
-		String dataPwd =  new SimpleHash("md5", password ,ByteSource.Util.bytes("123456"),2).toHex();
 
-
-//		System.out.println("admin1"+":admin123456 "+new SimpleHash("md5", "admin123456" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin2"+":admin234561 "+new SimpleHash("md5", "admin234561" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin3"+":admin345612 "+new SimpleHash("md5", "admin345612" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin4"+":admin456123 "+new SimpleHash("md5", "admin456123" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin5"+":admin561234 "+new SimpleHash("md5", "admin561234" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin6"+":admin612345 "+new SimpleHash("md5", "admin612345" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin7"+":admin654321 "+new SimpleHash("md5", "admin654321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin8"+":admin543216 "+new SimpleHash("md5", "admin543216" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin9"+":admin432165 "+new SimpleHash("md5", "admin432165" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin10"+":admin321654 "+new SimpleHash("md5", "admin321654" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin11"+":admin216543 "+new SimpleHash("md5", "admin216543" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin12"+":admin165432 "+new SimpleHash("md5", "admin165432" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin13"+":admin654321 "+new SimpleHash("md5", "admin654321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin14"+":admin123456 "+new SimpleHash("md5", "admin123456" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin15"+":admin234561 "+new SimpleHash("md5", "admin234561" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin16"+":admin345621 "+new SimpleHash("md5", "admin345621" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin17"+":admin345621 "+new SimpleHash("md5", "admin456321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin18"+":admin564321 "+new SimpleHash("md5", "admin564321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin19"+":admin654321 "+new SimpleHash("md5", "admin654321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin20"+":admin123456 "+new SimpleHash("md5", "admin123456" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin21"+":admin234561 "+new SimpleHash("md5", "admin234561" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin22"+":admin345612 "+new SimpleHash("md5", "admin345612" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin23"+":admin456321 "+new SimpleHash("md5", "admin456321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin24"+":admin564321 "+new SimpleHash("md5", "admin564321" ,ByteSource.Util.bytes("123456"),2).toHex());
-//		System.out.println("admin25"+":admin612345 "+new SimpleHash("md5", "admin612345" ,ByteSource.Util.bytes("123456"),2).toHex());
-
-
-
-
-
-		
-		if(!dataPwd.equals(pwd)){
-			return ResponseBean.errorResponse("false");
-		}
-		
-		request.getSession().setAttribute(Constants.SESSION.loginName, name);
-		
 		return ResponseBean.successResponse("data/showTables.do");
 		
 	}
-	
+
+	@RequestMapping("/logout")
+	@ResponseBody
+	public Object logout(HttpServletRequest request) {
+
+		Subject currentUser = SecurityUtils.getSubject();
+		logger.info(currentUser.getPrincipal()+"登出后台系统!");
+		currentUser.logout();
+
+		return ResponseBean.successResponse(true);
+	}
 }
