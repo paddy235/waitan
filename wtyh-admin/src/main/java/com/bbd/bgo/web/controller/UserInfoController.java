e81717ec5f63634c057fda75e699a63d4963a6c1
su <suyin@bbdservice.com>
2017-03-06 11:30:29 +0800

    添加跨域设置

--- wtyh-admin/src/main/java/com/bbd/bgo/web/controller/UserInfoController.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/web/controller/UserInfoController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 //import javax.servlet.http.HttpServletResponse;
-
 
 /**
  * Created by cgj on 2017/2/27.
@@ -25,141 +25,124 @@ import java.util.*;
 @RequestMapping("/userInfo")
 public class UserInfoController {
 
-	@Autowired
-	private UserInfoService uis;
+    @Autowired
+    private UserInfoService uis;
 
-	@RequestMapping("/createUser")
-	@ResponseBody
-	public Object createUser1(
-			UserInfoTableDo uitd,
-			@RequestParam String resourceSet,
-			HttpServletRequest request	) {
-		// hh.addHeader("aa","1234");
-		String loginName = (String)request.getSession().getAttribute(Constants.SESSION.loginName);
-		uitd.setCreateBy(loginName);
-		try {
-			uis.createUser(uitd, resourceSet);
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常");
-		}
-		return ResponseBean.successResponse("OK");
-	}
+    @RequestMapping("/createUser")
+    @ResponseBody
+    public Object createUser1(UserInfoTableDo uitd, @RequestParam String resourceSet, HttpServletRequest request) {
+        // hh.addHeader("aa","1234");
+        String loginName = (String) request.getSession().getAttribute(Constants.SESSION.loginName);
+        uitd.setCreateBy(loginName);
+        try {
+            uis.createUser(uitd, resourceSet);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常");
+        }
+        return ResponseBean.successResponse("OK");
+    }
 
-/*	@RequestMapping("/myUserLogin")
-	@ResponseBody
-	public Object myUserLogin(@RequestParam String loginName, @RequestParam String password, @RequestParam String type) {
-		return uis.compareUserNameMatchPassword(loginName, password, type);
-	}*/
+    /*	@RequestMapping("/myUserLogin")
+    	@ResponseBody
+    	public Object myUserLogin(@RequestParam String loginName, @RequestParam String password, @RequestParam String type) {
+    		return uis.compareUserNameMatchPassword(loginName, password, type);
+    	}*/
 
-	@RequestMapping("/updateUserInfo.do")
-	@ResponseBody
-	public Object updateUserInfo(
-			UserInfoTableDo uitd,
-			@RequestParam String resourceSet,
-			HttpServletRequest request ) {
-		String loginName = (String)request.getSession().getAttribute(Constants.SESSION.loginName);
-		uitd.setUpdateBy(loginName);
-		try {
-			uis.updateUserInfo(uitd, resourceSet);
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常");
-		}
-		return ResponseBean.successResponse("OK");
-	}
+    @RequestMapping("/updateUserInfo.do")
+    @ResponseBody
+    public Object updateUserInfo(UserInfoTableDo uitd, @RequestParam String resourceSet, HttpServletRequest request) {
+        String loginName = (String) request.getSession().getAttribute(Constants.SESSION.loginName);
+        uitd.setUpdateBy(loginName);
+        try {
+            uis.updateUserInfo(uitd, resourceSet);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常");
+        }
+        return ResponseBean.successResponse("OK");
+    }
 
-	@RequestMapping("/deleteUser.do")
-	@ResponseBody
-	public Object updateUserInfo(
-			@RequestParam Integer deleteId,
-			HttpServletRequest request ) {
-		try {
-			UserInfoTableDo uitd =new UserInfoTableDo();
-			String loginName = (String)request.getSession().getAttribute(Constants.SESSION.loginName);
-			uitd.setUpdateBy(loginName);
-			uitd.setId(deleteId);
-			uitd.setStatus("F"); //逻辑删除
-			uis.updateUserInfo(uitd, null);
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常：" +e);
-		}
-		return ResponseBean.successResponse("OK");
-	}
+    @RequestMapping("/deleteUser.do")
+    @ResponseBody
+    public Object updateUserInfo(@RequestParam Integer deleteId, HttpServletRequest request) {
+        try {
+            UserInfoTableDo uitd = new UserInfoTableDo();
+            String loginName = (String) request.getSession().getAttribute(Constants.SESSION.loginName);
+            uitd.setUpdateBy(loginName);
+            uitd.setId(deleteId);
+            uitd.setStatus("F"); //逻辑删除
+            uis.updateUserInfo(uitd, null);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常：" + e);
+        }
+        return ResponseBean.successResponse("OK");
+    }
 
-	@RequestMapping("/listUserInfo.do")
-	@ResponseBody
-	public Object listUserInfo(
-			@RequestParam String selectType,
-			String selectObject,
-			@RequestParam int pageLimit,
-			Integer pageOffset,
-			HttpServletRequest request ) {
-		Map<String,Object> rstMap =null;
-		try {
-			rstMap = uis.listUserInfo( selectType, selectObject, pageLimit, pageOffset );
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常：" +e);
-		}
-		return ResponseBean.successResponse(rstMap);
-	}
+    @RequestMapping("/listUserInfo.do")
+    @ResponseBody
+    public Object listUserInfo(@RequestParam String selectType, String selectObject, @RequestParam int pageLimit, Integer pageOffset, HttpServletRequest request) {
+        Map<String, Object> rstMap = null;
+        try {
+            rstMap = uis.listUserInfo(selectType, selectObject, pageLimit, pageOffset);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常：" + e);
+        }
+        return ResponseBean.successResponse(rstMap);
+    }
 
-	@RequestMapping("/queryUserInfoById.do")
-	@ResponseBody
-	public Object gueryUserInfoById(
-			@RequestParam int queryId,
-			HttpServletRequest request ) {
-		Map<String,Object> rstMap =null;
-		try {
-			rstMap = uis.getUserInfoById( queryId );
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常：" +e);
-		}
-		return ResponseBean.successResponse(rstMap);
-	}
+    @RequestMapping("/queryUserInfoById.do")
+    @ResponseBody
+    public Object gueryUserInfoById(@RequestParam int queryId, HttpServletRequest request) {
+        Map<String, Object> rstMap = null;
+        try {
+            rstMap = uis.getUserInfoById(queryId);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常：" + e);
+        }
+        return ResponseBean.successResponse(rstMap);
+    }
 
-	@RequestMapping("/gueryUserTemplate.do")
-	@ResponseBody
-	public Object gueryUserTemplate(
-			@RequestParam String loginName,
-			HttpServletRequest request ) {
-		List<Map<String, Object>> rstList =null;
-		try {
-			rstList = uis.getUserTemplate(loginName);
-		} catch (BusinessException be) {
-			return ResponseBean.errorResponse(be.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseBean.errorResponse("服务器异常：" +e);
-		}
-		return ResponseBean.successResponse(rstList);
-	}
+    @RequestMapping("/gueryUserTemplate.do")
+    @ResponseBody
+    public Object gueryUserTemplate(@RequestParam String loginName, HttpServletRequest request) {
+        List<Map<String, Object>> rstList = null;
+        try {
+            rstList = uis.getUserTemplate(loginName);
+        } catch (BusinessException be) {
+            return ResponseBean.errorResponse(be.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBean.errorResponse("服务器异常：" + e);
+        }
+        return ResponseBean.successResponse(rstList);
+    }
 
-	@RequestMapping("/myTest.do")
-	@ResponseBody
-	public Object myTest() {
-		//下面是测试代码
-		try {
-			Map<String,Object> rstMap = uis.listUserInfo("realName","ad", 10, null);
-			System.out.println(rstMap);
-			return rstMap;
-		} catch (Exception ee)	{
-			;
-		}
-		return  ResponseBean.successResponse( "err" );
-	}
+    @RequestMapping("/myTest.do")
+    @ResponseBody
+    public Object myTest() {
+        //下面是测试代码
+        try {
+            Map<String, Object> rstMap = uis.listUserInfo("realName", "ad", 10, null);
+            System.out.println(rstMap);
+            return rstMap;
+        } catch (Exception ee) {
+            ;
+        }
+        return ResponseBean.successResponse(new UserInfoTableDo());
+    }
 
 }
--- wtyh-web/src/main/java/com/bbd/wtyh/filter/CrossDomainFilter.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/filter/CrossDomainFilter.java
@@ -1,19 +1,14 @@
-package com.bbd.bgo.filter;
+/*
+ * BBD Service Inc
+ * All Rights Reserved @2017
+ */
 
-import java.io.IOException;
-import java.util.Properties;
+package com.bbd.wtyh.filter;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.context.ContextLoader;
-import org.springframework.web.context.WebApplicationContext;
+import java.io.IOException;
 
 public class CrossDomainFilter implements Filter {
 
