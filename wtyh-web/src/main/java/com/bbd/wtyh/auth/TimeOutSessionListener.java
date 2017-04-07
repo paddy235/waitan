385ad276d4122714390c9b52a16d195c623ff2c5
sunliming <sunliming@bbdservice.com>
2017-04-07 16:41:17 +0800

    角色权限

---
+++ wtyh-web/src/main/java/com/bbd/wtyh/auth/TimeOutSessionListener.java
@@ -0,0 +1,23 @@
+package com.bbd.wtyh.auth;
+
+import com.bbd.higgs.utils.StringUtils;
+import com.bbd.wtyh.log.user.Operation;
+import com.bbd.wtyh.log.user.UserLogRecord;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListenerAdapter;
+
+
+/**
+ * Created by cgj on 2017/4/6.
+ */
+public class TimeOutSessionListener extends SessionListenerAdapter {
+    @Override
+    public void onExpiration(Session session) {
+        //会话过期时触发
+        String name=(String)session.getAttribute("loginName");
+        if(!StringUtils.isNullOrEmpty(name)){
+            UserLogRecord.recordForShiroSession("用户登出(会话过期)", Operation.Type.logout, Operation.Page.blank, Operation.System.front,  session);
+        }
+    }
+
+}
