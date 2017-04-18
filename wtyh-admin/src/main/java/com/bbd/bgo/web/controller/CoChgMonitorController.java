80e91f90eccfeaa44e566ff56d075b7296c62e74
sunliming <sunliming@bbdservice.com>
2017-04-18 11:47:55 +0800

    企业变化检测

---
+++ wtyh-admin/src/main/java/com/bbd/bgo/web/controller/CoChgMonitorController.java
@@ -0,0 +1,13 @@
+package com.bbd.bgo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/4/18 0018.
+ */
+@Controller
+@RequestMapping("/coChgMonitor")
+public class CoChgMonitorController {
+
+}
