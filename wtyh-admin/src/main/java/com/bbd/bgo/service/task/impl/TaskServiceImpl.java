f5b57079f8db891d9e446890cd8097305262a522
liyao <liyao@bbdservice.com>
2017-04-26 11:17:17 +0800

    上海市信息中心接口

---
+++ wtyh-admin/src/main/java/com/bbd/bgo/service/task/impl/TaskServiceImpl.java
@@ -0,0 +1,16 @@
+package com.bbd.bgo.service.task.impl;
+
+import com.bbd.bgo.service.task.TaskService;
+
+/**
+ * 所有定时任务统一管理接口实现类
+ *
+ * @author Created by LiYao on 2017-04-25 14:43.
+ */
+public class TaskServiceImpl implements TaskService {
+
+	@Override
+	public void companyCreditScoreTask() {
+
+	}
+}
---
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/CoCreditScoreService.java
@@ -0,0 +1,14 @@
+package com.bbd.wtyh.service;
+
+/**
+ * 公司信用评分接口
+ *
+ * @author Created by LiYao on 2017-04-25 14:52.
+ */
+public interface CoCreditScoreService {
+
+	/**
+	 * 信用分值计算
+	 */
+	void creditScoreCalculate();
+}
