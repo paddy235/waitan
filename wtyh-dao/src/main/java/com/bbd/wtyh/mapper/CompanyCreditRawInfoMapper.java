de70bd17cacf75d882d7109a19e40a7cf8b6006e
chengongjie <chengongjie@bbdservice.com>
2017-05-04 18:05:24 +0800

    添加上海信息中心数据入库

---
+++ wtyh-dao/src/main/java/com/bbd/wtyh/mapper/CompanyCreditRawInfoMapper.java
@@ -0,0 +1,25 @@
+package com.bbd.wtyh.mapper;
+
+import com.bbd.wtyh.domain.CompanyCreditRawInfoDO;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+
+/**
+ * Created by cgj on 2017/5/4.
+ */
+
+public interface CompanyCreditRawInfoMapper {
+
+	@Insert("INSERT INTO company_credit_raw_info(" +
+			"`company_name`, `organization_code`, `credit_code`, `cxbh`, `resources`, `resource_name`, `resource_code`," +
+			"`xxssdw_code`, `xxssdw`, `xxlb`, `content`, `create_by`, `create_date` ) VALUES(" +
+			"#{companyName}, #{organizationCode}, #{creditCode}, #{cxbh}, #{resources}, #{resourceName}, #{resourceCode}," +
+			"#{xxssdwCode}, #{xxssdw}, #{xxlb}, #{content}, 'system', NOW() )")
+	@Options(useGeneratedKeys =true, keyProperty = "id")
+	int saveCompanyCreditRawInfo(CompanyCreditRawInfoDO companyCreditRawInfoDo);
+
+	@Delete( "DELETE FROM `company_credit_raw_info` WHERE `company_name` =#{companyName}" )
+	int removeCompanyCreditRawInfoByCompanyName( String companyName );
+
+}
