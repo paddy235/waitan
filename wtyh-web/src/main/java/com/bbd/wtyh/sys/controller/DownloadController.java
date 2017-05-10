ac22f7f011250a080bda146fab685fb9bddf8834
liyao <liyao@bbdservice.com>
2017-05-10 16:10:32 +0800

    下载文件名字

--- wtyh-web/src/main/java/com/bbd/wtyh/sys/controller/DownloadController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/sys/controller/DownloadController.java
@@ -2,7 +2,9 @@ package com.bbd.wtyh.sys.controller;
 
 import java.io.*;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
@@ -25,26 +27,37 @@ public class DownloadController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DownloadController.class);
 
 	@RequestMapping("/download-excel")
-	public void downloadExcel(@RequestParam String name, HttpServletResponse response) {
+	public void downloadExcel(@RequestParam String name, HttpServletRequest request, HttpServletResponse response) {
 		String path = ExportExcelUtil.dealWithExportPath("");
 
 		InputStream in = null;
 		OutputStream out = null;
-
+		File file = null;
 		try {
 			name = URLDecoder.decode(name, "UTF-8");
 
-			File file = new File(path + name);
+			file = new File(path + name);
 
 			response.setCharacterEncoding("UTF-8");
+			String userAgent = request.getHeader("User-Agent");
 
 			if (!file.exists()) {
 				out = response.getOutputStream();
-				out.write("导出文件失败!".getBytes("GB2312"));
+				if (userAgent.toUpperCase().contains("MSIE")) {
+					out.write("导出文件失败!".getBytes("GBK"));
+				} else {
+					out.write("导出文件失败!".getBytes("UTF-8"));
+				}
 				LOGGER.error("导出文件【{}】失败：文件不存在", name);
 				return;
 			}
-			String filename = new String(name.getBytes("GB2312"), "ISO-8859-1");
+
+			String filename;
+			if (userAgent.toUpperCase().contains("MSIE")) {
+				filename = new String(name.getBytes("GBK"), "ISO-8859-1");
+			} else {
+				filename = new String(name.getBytes("UTF-8"), "ISO-8859-1");
+			}
 			response.setContentType("multipart/form-data");
 			response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
 
@@ -55,6 +68,7 @@ public class DownloadController {
 			while ((length = in.read(b)) > 0) {
 				out.write(b, 0, length);
 			}
+
 		} catch (IOException e) {
 			LOGGER.error("导出文件【{}】失败：服务器报错!{}", name, e);
 		} finally {
@@ -67,6 +81,9 @@ public class DownloadController {
 				}
 			} catch (IOException e) {
 				// e.printStackTrace();
+			}
+			if (file != null) {
+				file.delete();
 			}
 		}
 
