eb05f8aa7f7c62194643c02be8b7910b4ef04dc9
chengongjie <chengongjie@bbdservice.com>
2017-06-02 18:22:11 +0800

    cgj-add-userinfo-1

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ReportDocxController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ReportDocxController.java
@@ -1,7 +1,8 @@
 package com.bbd.wtyh.web.controller;
 
-import java.util.List;
+import java.io.InputStream;
 
+import com.bbd.wtyh.util.DocxUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,33 +10,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bbd.wtyh.service.AreaService;
 import com.bbd.wtyh.web.ResponseBean;
-import com.google.common.collect.Lists;
 
 /**
- * 地区相关接口
+ * word报告文档导出
+ *  Created by cgj on 2017-5-25
  */
 @Controller
-@RequestMapping("/area/")
+@RequestMapping("/reportDocx/")
 public class ReportDocxController {
 	
 	@Autowired
 	private AreaService areaService;
 
-	/**
-	 * 获取父节点下面的数据
-	 * 比如属于上海市的所有区 parentId:104
-	 * @param areaId
-	 * @return
-     */
-	@RequestMapping(value = "getAreaByParentId.do")
+
+	@RequestMapping(value = "t1.do")
 	@ResponseBody
-	public ResponseBean getAreaByParentId(Integer areaId) {
-		//产品提出要排序，不读库的方式，写死
-		List<String> list = Lists.newArrayList("浦东新区","黄浦区","静安区","徐汇区",
-												"长宁区","普陀区","虹口区","杨浦区",
-												"宝山区","闵行区","嘉定区","金山区",
-												"松江区","青浦区","奉贤区","崇明县");
-		return ResponseBean.successResponse(list);
+	public ResponseBean getAreaByParentId() {
+		InputStream aa = DocxUtils.class.getResourceAsStream("/config/shanghai-company-anti-fraud-template.docx");
+		byte [] te = new byte[100];
+		try {
+			int aar = aa.read(te);
+			aar ++;
+		} catch (Exception e) {}
+		return ResponseBean.successResponse(null);
 	}
 
 }
