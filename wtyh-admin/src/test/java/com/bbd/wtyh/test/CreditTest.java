cd3582e01256b26cd32d75e2eb544f6496b00624
sunliming <sunliming@bbdservice.com>
2017-06-14 12:55:25 +0800

    公信

--- wtyh-admin/src/test/java/com/bbd/wtyh/test/CreditTest.java
+++ wtyh-admin/src/test/java/com/bbd/wtyh/test/CreditTest.java
@@ -2,7 +2,9 @@ package com.bbd.wtyh.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.bbd.wtyh.domain.CompanyCreditRawInfoDO;
 import com.bbd.wtyh.domain.credit.CompanyCreditFailInfoDO;
+import com.bbd.wtyh.mapper.CompanyCreditRawInfoMapper;
 import com.bbd.wtyh.service.CoCreditScoreService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,11 +23,21 @@ public class CreditTest {
 
     @Autowired
     private CoCreditScoreService coCreditScoreService;
+    @Autowired
+    private CompanyCreditRawInfoMapper companyCreditRawInfoMapper;
     @Test
     public void queryfailCompanyTest(){
         List<CompanyCreditFailInfoDO> list=coCreditScoreService.queryfailCompany(
                 new String[]{}, "1002", "20170608",1,2);
         System.err.println(JSON.toJSONString(list, SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect));
+
+    }
+
+    @Test
+    public void insertTest(){
+        CompanyCreditRawInfoDO companyCreditRawInfoDO=new CompanyCreditRawInfoDO();
+        companyCreditRawInfoDO.setCompanyName("你好5");
+        companyCreditRawInfoMapper.saveCompanyCreditRawInfo(companyCreditRawInfoDO);
 
     }
 }
