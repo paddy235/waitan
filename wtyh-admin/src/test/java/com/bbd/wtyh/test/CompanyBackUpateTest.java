5ca52633b29d5e0314306e8a1549af73f452371b
lixudong <839444394@qq.com>
2017-07-06 11:13:00 +0800

    更新background表

---
+++ wtyh-admin/src/test/java/com/bbd/wtyh/test/CompanyBackUpateTest.java
@@ -0,0 +1,24 @@
+package com.bbd.wtyh.test;
+
+import com.bbd.bgo.service.task.SystemDataUpdateService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by lixudong on 2017/7/5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-config.xml" })
+public class CompanyBackUpateTest {
+
+    @Autowired
+    private SystemDataUpdateService systemDataUpdateService;
+
+    @Test
+    public void backgroundTest(){
+        systemDataUpdateService.updateCompanyTableAreaIdAndAddress();
+    }
+}
