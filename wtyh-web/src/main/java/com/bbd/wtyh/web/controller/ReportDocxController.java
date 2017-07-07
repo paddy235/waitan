a19fd46de64aa17762fc6ed5f72761174f3cbf87
chengongjie <chengongjie@bbdservice.com>
2017-07-07 16:54:48 +0800

    fix IE download err

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ReportDocxController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/ReportDocxController.java
@@ -1,6 +1,7 @@
 package com.bbd.wtyh.web.controller;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.Map;
 
@@ -8,6 +9,9 @@ import com.bbd.wtyh.common.Constants;
 import com.bbd.wtyh.log.user.Operation;
 import com.bbd.wtyh.log.user.annotation.LogRecord;
 import com.bbd.wtyh.report.service.WordReportService;
+import com.bbd.wtyh.sys.controller.DownloadController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bbd.wtyh.web.ResponseBean;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * word报告文档导出
@@ -33,10 +38,12 @@ public class ReportDocxController {
 	@Autowired
 	private WordReportService wordReportService;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(DownloadController.class);
+
+	//  这个代码不能支持IE浏览器的下载，火狐或谷歌下工作正常
 	@LogRecord(logMsg = "导出“%s”的企业全息报告", params = {"companyName"}, page = Operation.Page.hologram,
 			type = Operation.Type.REPORT_EXPORT, after = true, before = false)
-	@RequestMapping( value = "download.do" )
-	@ResponseBody
+	@RequestMapping( value = "download-noIE.do" )
 	public Object downloadFile(HttpServletRequest request,
                                @RequestParam String companyName )  {
         //Subject currentUser = SecurityUtils.getSubject(); currentUser.getSession().getAttribute(Constants.SESSION.loginName);
@@ -49,10 +56,48 @@ public class ReportDocxController {
 					"iso-8859-1");
 			headers.setContentDispositionFormData("attachment", downloadFilename);
 			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-			return  new ResponseEntity( rstMap.get("fileBytes"), headers, HttpStatus.CREATED ) ; /*ResponseEntity<byte[]>*/
+			return  new ResponseEntity( rstMap.get("fileBytes"), headers, HttpStatus.CREATED ) ;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseBean.errorResponse("服务器异常！");
+		}
+	}
+
+	@LogRecord(logMsg = "导出“%s”的企业全息报告", params = {"companyName"}, page = Operation.Page.hologram,
+			type = Operation.Type.REPORT_EXPORT, after = true, before = false)
+	@RequestMapping( value = "download.do" )
+	public void downloadFile(HttpServletRequest request,
+							   HttpServletResponse response,
+							   @RequestParam String companyName )  {
+		OutputStream out = null;
+		try {
+			String loginName = (String) request.getSession().getAttribute(Constants.SESSION.loginName);
+			String areaCode = (String) request.getSession().getAttribute("area");
+			Map<String, Object> rstMap = wordReportService.reportExport(companyName, loginName, areaCode);
+
+			companyName =(String) rstMap.get("fileName");
+			response.setCharacterEncoding("UTF-8");
+			String userAgent = request.getHeader("User-Agent");
+			String filename;
+			if (userAgent.toUpperCase().contains("MSIE")) {
+				filename = new String(companyName.getBytes("GBK"), "ISO-8859-1");
+			} else {
+				filename = new String(companyName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+			response.setContentType("multipart/form-data");
+			response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+			byte[] fileBytes =(byte[]) rstMap.get("fileBytes");
+			out = response.getOutputStream();
+			out.write(fileBytes, 0, fileBytes.length);
+		} catch (Exception e) {
+			LOGGER.error("导出文件【{}】失败：服务器报错!{}", companyName, e);
+		} finally {
+			try {
+				if( null != out ) {
+					out.close();
+				}
+
+			} catch (Exception e) {}
 		}
 	}
 
