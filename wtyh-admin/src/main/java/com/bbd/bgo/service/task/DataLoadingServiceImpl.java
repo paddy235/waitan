a2ba04ae40ce93fd8032c8aea2d3165f210b2c34
hongwen <827143084@qq.com>
2017-07-10 17:08:34 +0800

    数据落地bug修改

--- wtyh-admin/src/main/java/com/bbd/bgo/service/task/DataLoadingServiceImpl.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/service/task/DataLoadingServiceImpl.java
@@ -1,8 +1,10 @@
 package com.bbd.bgo.service.task;
 
 import com.bbd.wtyh.core.base.BaseServiceImpl;
-import com.bbd.wtyh.domain.dataLoading.DishonestyDO;
+import com.bbd.wtyh.domain.TaskSuccessFailInfoDO;
+import com.bbd.wtyh.domain.dataLoading.*;
 import com.bbd.wtyh.mapper.DataLoadingMapper;
+import com.bbd.wtyh.mapper.TaskSuccessFailInfoMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import net.sf.json.JSONObject;
@@ -12,8 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * wtyh
@@ -27,21 +28,240 @@ public class DataLoadingServiceImpl extends BaseServiceImpl implements DataLoadi
 
 	private static final String FILE_PATH = "C:\\\\Users\\\\ibm\\\\Desktop\\\\changjiang_20170704_test.test";
 
+	private static String TASK_NAME = "shangHaiCreditJob";
+
+	private static String TASK_GROUP = "data_loading";
+
     @Autowired
     private DataLoadingMapper dataLoadingMapper;
+	@Autowired
+	private TaskSuccessFailInfoMapper taskSuccessFailInfoMapper;
 
 
 	@Override
 	public void dataLoading() {
+		//taskSuccessFailInfoMapper.getTaskSuccessFailInfo()
 		List<String> list = txt2String(new File(FILE_PATH));
-		JSONObject jsonObject = JSONObject.fromObject(list.get(0));
-		Object data = jsonObject.get("data");
-		String tn = String.valueOf(jsonObject.get("tn"));
-		JSONObject jsonData = JSONObject.fromObject(String.valueOf(data));
-		Object dishonesty = jsonData.get(tn);
 		Gson gson = new Gson();
-		List<DishonestyDO> dishonestyList = gson.fromJson(String.valueOf(dishonesty), new TypeToken<List<DishonestyDO>>() {}.getType());
-        dataLoadingMapper.saveDishonestyDO(dishonestyList);
+		List<DishonestyDO> disList=new ArrayList<DishonestyDO>();
+		List<KtggDO> ktggList=new ArrayList<KtggDO>();
+		List<QyxgYuqingDO> yuQingList=new ArrayList<QyxgYuqingDO>();
+		List<QyxxBasicDO> basicList=new ArrayList<QyxxBasicDO>();
+		List<QyxxBaxxDO> baxxList=new ArrayList<QyxxBaxxDO>();
+		List<QyxxGdxxDO> gdxxList=new ArrayList<QyxxGdxxDO>();
+		List<QyxxZhuanliDO> zhuanliList=new ArrayList<QyxxZhuanliDO>();
+		List<RmfyggDO> rmfyggList=new ArrayList<RmfyggDO>();
+		List<ZgcpwswDO> zgcpwswList=new ArrayList<ZgcpwswDO>();
+		List<ZhixingDO> zhixingList=new ArrayList<ZhixingDO>();
+		for(String s:list){
+			JSONObject jsonObject = JSONObject.fromObject(s);
+			Object data = jsonObject.get("data");
+			String tn = String.valueOf(jsonObject.get("tn"));
+			String bbd_qyxx_id = String.valueOf(jsonObject.get("bbd_qyxx_id"));
+			JSONObject jsonData = JSONObject.fromObject(String.valueOf(data));
+			Object dataName = jsonData.get(tn);
+			String dataStr = String.valueOf(dataName);
+			addDataToList(gson,tn,dataStr,bbd_qyxx_id,disList,ktggList,yuQingList,basicList,baxxList,gdxxList,
+					zhuanliList,rmfyggList,zgcpwswList,zhixingList);
+		}
+		Map<String,Integer> successMap = insertData(disList,ktggList,yuQingList,basicList,baxxList,gdxxList,
+				zhuanliList,rmfyggList,zgcpwswList,zhixingList);
+		int successNum = 0;
+		for (Integer value : successMap.values()) {
+			successNum+=value;
+		}
+		int failNum = 10-successNum;
+		TaskSuccessFailInfoDO taskSuccessFailInfoDO = new TaskSuccessFailInfoDO();
+		taskSuccessFailInfoDO.setTaskName(TASK_NAME);
+		taskSuccessFailInfoDO.setTaskGroup(TASK_GROUP);
+		taskSuccessFailInfoDO.setSuccessCount(successNum);
+		taskSuccessFailInfoDO.setFailCount(failNum);
+		taskSuccessFailInfoDO.setCreateBy("system");
+		taskSuccessFailInfoDO.setCreateDate(new Date());
+		int id = taskSuccessFailInfoMapper.addTaskSuccessFailInfo(taskSuccessFailInfoDO);
+	}
+
+	public Map<String,Integer> insertData(List<DishonestyDO> disList,List<KtggDO> ktggList,
+						   List<QyxgYuqingDO> yuQingList,List<QyxxBasicDO> basicList,List<QyxxBaxxDO> baxxList, List<QyxxGdxxDO> gdxxList,
+						   List<QyxxZhuanliDO> zhuanliList,List<RmfyggDO> rmfyggList, List<ZgcpwswDO> zgcpwswList,List<ZhixingDO> zhixingList){
+		Map<String,Integer> map=new HashMap<String,Integer>();
+		map.put("dis",1);
+		map.put("ktgg",1);
+		map.put("yuqing",1);
+		map.put("basic",1);
+		map.put("baxx",1);
+		map.put("gdxx",1);
+		map.put("zhunali",1);
+		map.put("zgcpwsw",1);
+		map.put("zhixing",1);
+		map.put("rmfygg",1);
+		if(disList.size()>0){
+			try {
+				dataLoadingMapper.saveDishonestyDO(disList);
+			} catch (Exception e) {
+				map.put("dis",0);
+				e.printStackTrace();
+			}
+		}
+		if(ktggList.size()>0){
+			try {
+				dataLoadingMapper.saveKtggDO(ktggList);
+			} catch (Exception e) {
+				map.put("ktgg",0);
+				e.printStackTrace();
+			}
+		}
+		if(yuQingList.size()>0){
+			try {
+				dataLoadingMapper.saveQyxgYuqingDO(yuQingList);
+			} catch (Exception e) {
+				map.put("yuqing",0);
+				e.printStackTrace();
+			}
+		}
+		if(basicList.size()>0){
+			try {
+				dataLoadingMapper.saveQyxxBasicDO(basicList);
+			} catch (Exception e) {
+				map.put("basic",0);
+				e.printStackTrace();
+			}
+		}
+
+		if(baxxList.size()>0){
+			try {
+				dataLoadingMapper.saveQyxxBaxxDO(baxxList);
+			} catch (Exception e) {
+				map.put("baxx",0);
+				e.printStackTrace();
+			}
+		}
+
+		if(gdxxList.size()>0){
+			try {
+				dataLoadingMapper.saveQyxxGdxxDO(gdxxList);
+			} catch (Exception e) {
+				map.put("gdxx",0);
+				e.printStackTrace();
+			}
+		}
+
+		if(zhuanliList.size()>0){
+			try {
+				dataLoadingMapper.saveQyxxZhuanliDO(zhuanliList);
+			} catch (Exception e) {
+				map.put("zhunali",0);
+				e.printStackTrace();
+			}
+		}
+		if(rmfyggList.size()>0) {
+			try {
+				dataLoadingMapper.saveRmfyggDO(rmfyggList);
+			} catch (Exception e) {
+				map.put("rmfygg",0);
+				e.printStackTrace();
+			}
+		}
+		if(zgcpwswList.size()>0){
+			try {
+				dataLoadingMapper.saveZgcpwswDO(zgcpwswList);
+			} catch (Exception e) {
+				map.put("zgcpwsw",0);
+				e.printStackTrace();
+			}
+		}
+
+		if(zhixingList.size()>0){
+			try {
+				dataLoadingMapper.saveZhixingDO(zhixingList);
+			} catch (Exception e) {
+				map.put("zhixing",10);
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+
+	public void addDataToList(Gson gson,String tn,String dataStr,String bbd_qyxx_id,List<DishonestyDO> disList,List<KtggDO> ktggList,
+							  List<QyxgYuqingDO> yuQingList,List<QyxxBasicDO> basicList,List<QyxxBaxxDO> baxxList, List<QyxxGdxxDO> gdxxList,
+							  List<QyxxZhuanliDO> zhuanliList,List<RmfyggDO> rmfyggList, List<ZgcpwswDO> zgcpwswList,List<ZhixingDO> zhixingList){
+		switch (tn){
+			case "dishonesty" :
+				List<DishonestyDO> dataList1 = gson.fromJson(dataStr, new TypeToken<List<DishonestyDO>>() {}.getType());
+				for(DishonestyDO data:dataList1){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				disList.addAll(dataList1);
+				break;
+			case "ktgg" :
+				List<KtggDO> dataList2 = gson.fromJson(dataStr, new TypeToken<List<KtggDO>>() {}.getType());
+				for(KtggDO data:dataList2){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				ktggList.addAll(dataList2);
+				break;
+			case "qyxg_yuqing" :
+				List<QyxgYuqingDO> dataList3 = gson.fromJson(dataStr, new TypeToken<List<QyxgYuqingDO>>() {}.getType());
+				for(QyxgYuqingDO data:dataList3){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				yuQingList.addAll(dataList3);
+				break;
+			case "qyxx_basic" :
+				List<QyxxBasicDO> dataList4 = gson.fromJson(dataStr, new TypeToken<List<QyxxBasicDO>>() {}.getType());
+				for(QyxxBasicDO data:dataList4){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+					StringBuffer history_name = new StringBuffer();
+					for(String s:data.getBbd_history_name()){
+						history_name.append(s);
+					}
+					data.setBbd_history_names(history_name.toString());
+				}
+				basicList.addAll(dataList4);
+				break;
+			case "qyxx_baxx" :
+				List<QyxxBaxxDO> dataList5 = gson.fromJson(dataStr, new TypeToken<List<QyxxBaxxDO>>() {}.getType());
+				for(QyxxBaxxDO data:dataList5){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				baxxList.addAll(dataList5);
+				break;
+			case "qyxx_gdxx" :
+				List<QyxxGdxxDO> dataList6 = gson.fromJson(dataStr, new TypeToken<List<QyxxGdxxDO>>() {}.getType());
+				for(QyxxGdxxDO data:dataList6){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				gdxxList.addAll(dataList6);
+				break;
+			case "qyxx_zhuanli" :
+				List<QyxxZhuanliDO> dataList7 = gson.fromJson(dataStr, new TypeToken<List<QyxxZhuanliDO>>() {}.getType());
+				for(QyxxZhuanliDO data:dataList7){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				zhuanliList.addAll(dataList7);
+				break;
+			case "rmfygg" :
+				List<RmfyggDO> dataList8 = gson.fromJson(dataStr, new TypeToken<List<RmfyggDO>>() {}.getType());
+				for(RmfyggDO data:dataList8){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				rmfyggList.addAll(dataList8);
+				break;
+			case "zgcpwsw" :
+				List<ZgcpwswDO> dataList9 = gson.fromJson(dataStr, new TypeToken<List<ZgcpwswDO>>() {}.getType());
+				for(ZgcpwswDO data:dataList9){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				zgcpwswList.addAll(dataList9);
+				break;
+			case "zhixing" :
+				List<ZhixingDO> dataList10 = gson.fromJson(dataStr, new TypeToken<List<ZhixingDO>>() {}.getType());
+				for(ZhixingDO data:dataList10){
+					data.setBbd_qyxx_id(bbd_qyxx_id);
+				}
+				zhixingList.addAll(dataList10);
+				break;
+		}
 	}
 
 	public List<String> txt2String(File file){
