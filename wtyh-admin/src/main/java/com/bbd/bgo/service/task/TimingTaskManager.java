5323ec8b5b988fc6b7185b529ba209ff70b17c9e
lixudong <839444394@qq.com>
2017-07-19 14:31:41 +0800

    网贷之家 任务监控

--- wtyh-admin/src/main/java/com/bbd/bgo/service/task/TimingTaskManager.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/service/task/TimingTaskManager.java
@@ -232,7 +232,7 @@ public class TimingTaskManager {
 			Integer runMode = 0;// 运行方式：0 自动执行， 1 手动执行
 			taskId=TaskUtil.taskStart(TaskUtil.pToPMonitorJob[0],TaskUtil.pToPMonitorJob[1],dataVersion,runMode,null,null);
 			//需要传 taskId 给业务接口
-			map = pToPMonitorService.industryShanghaiDataLandingTask();
+			map = pToPMonitorService.pToPMonitorDataLanding(taskId);
 //			pToPMonitorService.industryCompareDataLandingTask();
 //			pToPMonitorService.platRankDataLandingTask();
 
