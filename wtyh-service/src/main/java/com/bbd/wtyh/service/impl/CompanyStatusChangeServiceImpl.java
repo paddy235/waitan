5fd53a9a2659c1af07e7d2a928adfe7fccfbbc05
yanwenyuan <yanwenyuan@bbdservice.com>
2017-07-27 11:01:28 +0800

    debug

--- wtyh-service/src/main/java/com/bbd/wtyh/service/impl/CompanyStatusChangeServiceImpl.java
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/impl/CompanyStatusChangeServiceImpl.java
@@ -16,6 +16,7 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.text.ParseException;
@@ -28,6 +29,7 @@ import java.util.Map;
 /**
  * Created by Administrator on 2017/7/19 0019.
  */
+@Service("companyStatusChangeService")
 public class CompanyStatusChangeServiceImpl implements CompanyStatusChangeService{
 
     @Autowired
