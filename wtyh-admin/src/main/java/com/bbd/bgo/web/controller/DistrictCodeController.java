9ab054c525c037105bc2d221229494953c5b8437
chengongjie <chengongjie@bbdservice.com>
2017-08-02 14:05:29 +0800

    bug fix -14

--- wtyh-admin/src/main/java/com/bbd/bgo/web/controller/DistrictCodeController.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/web/controller/DistrictCodeController.java
@@ -3,6 +3,9 @@ package com.bbd.bgo.web.controller;
 import java.util.Set;
 
 import com.bbd.wtyh.service.DistrictCodeService;
+import com.bbd.wtyh.service.impl.DistrictCodeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +19,20 @@ import com.bbd.wtyh.web.ResponseBean;
 @RequestMapping("/DistrictCode")
 public class DistrictCodeController {
 
+	private Logger log = LoggerFactory.getLogger(DistrictCodeController.class);
+
 	@Autowired
 	private DistrictCodeService districtCodeService;
 
 	@RequestMapping("/import.do")
 	@ResponseBody
-	public Object flushAll() {
-		return ResponseBean.successResponse( districtCodeService.ImportDistrictCode() );
+	public Object importCode() {
+		try {
+			return ResponseBean.successResponse( districtCodeService.ImportDistrictCode() );
+		} catch (Exception e) {
+			log.error( e.getMessage() );
+			return ResponseBean.errorResponse("服务器内部错误。");
+		}
 	}
 
 }
