e769e8e24733db746ab9fddcfd1eddee4eeb04d3
liyao <liyao@bbdservice.com>
2017-08-15 17:54:50 +0800

    网络借贷，1234，abcd 统一改为中文描述

--- wtyh-domain/src/main/java/com/bbd/wtyh/domain/enums/WangDaiRiskLevel.java
+++ wtyh-domain/src/main/java/com/bbd/wtyh/domain/enums/WangDaiRiskLevel.java
@@ -10,16 +10,16 @@ import java.util.Map;
  */
 public enum WangDaiRiskLevel {
 
-	RISK(1,"问题及停业平台","D"),
-	IMPORT_FOCUS(2,"重点关注","C"),
-	COMMON_FOCUS(3,"一般关注","B"),
-	NORMAL(4,"优良","A");
+	RISK(1, "问题及停业平台", "D"),
+	IMPORT_FOCUS(2, "重点关注", "C"),
+	COMMON_FOCUS(3, "一般关注", "B"),
+	NORMAL(4, "优良", "A");
 
 	private int type;
 	private String desc;
 	private String score;
 
-	WangDaiRiskLevel(int type, String desc,String score) {
+	WangDaiRiskLevel(int type, String desc, String score) {
 		this.type = type;
 		this.desc = desc;
 		this.score = score;
@@ -37,10 +37,10 @@ public enum WangDaiRiskLevel {
 		return this.score;
 	}
 
-	public static Map<String, Integer> getMap() {
-		Map<String, Integer> rst = new HashMap();
+	public static Map<String, String> getMap() {
+		Map<String, String> rst = new HashMap<>();
 		for (WangDaiRiskLevel wangDaiRiskLevel : WangDaiRiskLevel.values()) {
-			rst.put(wangDaiRiskLevel.desc,wangDaiRiskLevel.type());
+			rst.put(wangDaiRiskLevel.desc, wangDaiRiskLevel.desc());
 		}
 		return rst;
 	}
