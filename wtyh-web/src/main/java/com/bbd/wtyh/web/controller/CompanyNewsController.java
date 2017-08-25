54458884328b896b007200124cd94a052f2915a0
sunliming <sunliming@bbdservice.com>
2017-08-25 10:53:52 +0800

    格式化测试

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/CompanyNewsController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/CompanyNewsController.java
@@ -1,41 +1,37 @@
 package com.bbd.wtyh.web.controller;
 
-import com.bbd.wtyh.domain.vo.NewsVO;
-import com.bbd.wtyh.service.CompanyNewsService;
-import com.bbd.wtyh.web.*;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
+import com.bbd.wtyh.domain.vo.NewsVO;
+import com.bbd.wtyh.service.CompanyNewsService;
+import com.bbd.wtyh.web.ResponseBean;
+import com.google.gson.Gson;
 
 /**
-* 公司舆情
-* @author tracy zhou
-* @since  2016.8.12
-*/
+ * 公司舆情
+ * 
+ * @author tracy zhou
+ * @since 2016.8.12
+ */
 @RestController
 @RequestMapping("/companyNews")
 public class CompanyNewsController {
-	
-	@Autowired
-	private CompanyNewsService companyNewsService;
 
-	@RequestMapping("getCompanyNews.do")
-	@ResponseBody
-	public ResponseBean getCompanyNews() {
+    @Autowired
+    private CompanyNewsService companyNewsService;
 
-		NewsVO vo = companyNewsService.mutilTypeNews("qyxg_weiyangwang," +
-				                                     "qyxg_shanghai_finance," +
-				                                     "qyxg_chinesefinance," +
-				                                     "qyxg_financial_times," +
-				                                     "qyxg_sinafinance," +
-				                                     "qyxg_national_economy",20);
+    @RequestMapping("getCompanyNews.do")
+    @ResponseBody
+    public ResponseBean getCompanyNews() {
 
-		Gson gson = new Gson();
+        NewsVO vo = companyNewsService.mutilTypeNews("qyxg_weiyangwang," + "qyxg_shanghai_finance," + "qyxg_chinesefinance,"
+                + "qyxg_financial_times," + "qyxg_sinafinance," + "qyxg_national_economy", 20);
 
-		return ResponseBean.successResponse(gson.toJson(vo));
+        Gson gson = new Gson();
 
-	}
+        return ResponseBean.successResponse(gson.toJson(vo));
+    }
 }
