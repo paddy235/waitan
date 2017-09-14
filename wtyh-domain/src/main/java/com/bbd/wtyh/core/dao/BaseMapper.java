e5ff09a5a5f23cf06656435547c987b191d83798
liyao <liyao@bbdservice.com>
2017-09-14 17:17:42 +0800

    MybatisResultInterceptor 调整

--- wtyh-domain/src/main/java/com/bbd/wtyh/core/dao/BaseMapper.java
+++ wtyh-domain/src/main/java/com/bbd/wtyh/core/dao/BaseMapper.java
@@ -2,7 +2,9 @@ package com.bbd.wtyh.core.dao;
 
 import com.bbd.wtyh.core.entity.Pagination;
 import com.bbd.wtyh.core.mybatis.CRUDTemplate;
+import com.bbd.wtyh.core.mybatis.annotation.ResultIntercept;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -11,90 +13,86 @@ import java.util.List;
  *
  * @author Created by LiYao on 2017-03-03 22:40.
  */
+@Repository
+@ResultIntercept
 public interface BaseMapper {
 
-	/**
-	 * 根据条件查询全部，不支持实体类包含子类的
-	 *
-	 * @param clazz
-	 * @param where
-	 * @return
-	 */
-	@SelectProvider(type = CRUDTemplate.class, method = "select")
-	<T> List<T> baseSelectAll(@Param("clazz") Class<T> clazz, @Param("where") String where, @Param("params") Object... params);
+    /**
+     * 根据条件查询全部，不支持实体类包含子类的
+     *
+     * @param clazz
+     * @param where
+     * @return
+     */
+    @SelectProvider(type = CRUDTemplate.class, method = "select")
+    <T> List<T> selectAll(@Param("returnTypeClass") Class<T> clazz, @Param("where") String where, @Param("params") Object... params);
 
-	/**
-	 * 根据ID查询实体类
-	 *
-	 * @param clazz
-	 * @param id
-	 * @return
-	 */
-	@SelectProvider(type = CRUDTemplate.class, method = "selectById")
-	<T> List<T> baseSelectById(@Param("clazz") Class<T> clazz, @Param("id") Object id);
+    /**
+     * 根据ID查询实体类
+     *
+     * @param clazz
+     * @param id
+     * @return
+     */
+    @SelectProvider(type = CRUDTemplate.class, method = "selectById")
+    <T> List<T> selectById(@Param("returnTypeClass") Class<T> clazz, @Param("id") Object id);
 
-	/**
-	 * 根据条件查询全部（分页），实体类包含子类的不支持
-	 *
-	 * @param pagination
-	 *            分页组件
-	 * @param clazz
-	 *            实体类对象
-	 * @param where
-	 *            查询条件
-	 * @return
-	 */
-	@SelectProvider(type = CRUDTemplate.class, method = "select")
-	<T> List<T> baseSelectByPage(@Param("clazz") Class<T> clazz, @Param("pagination") Pagination pagination, @Param("where") String where,
-			@Param("params") Object... params);
+    /**
+     * 根据条件查询全部（分页），实体类包含子类的不支持
+     *
+     * @param pagination 分页组件
+     * @param clazz 实体类对象
+     * @param where 查询条件
+     * @return
+     */
+    @SelectProvider(type = CRUDTemplate.class, method = "select")
+    <T> List<T> selectByPage(@Param("returnTypeClass") Class<T> clazz, @Param("pagination") Pagination pagination,
+            @Param("where") String where, @Param("params") Object... params);
 
-	/**
-	 * 插入一条数据
-	 * 
-	 * @param obj
-	 * @return
-	 */
-	@InsertProvider(type = CRUDTemplate.class, method = "insert")
-	@Options(useGeneratedKeys = true)
-	<T> int insert(T obj);
+    /**
+     * 插入一条数据
+     * 
+     * @param obj
+     * @return
+     */
+    @InsertProvider(type = CRUDTemplate.class, method = "insert")
+    @Options(useGeneratedKeys = true)
+    <T> int insert(T obj);
 
-	/**
-	 * 插入多条数据
-	 *
-	 * @param list
-	 * @return
-	 */
-	@InsertProvider(type = CRUDTemplate.class, method = "insertList")
-	<T> int insertList(@Param("list") List<T> list);
+    /**
+     * 插入多条数据
+     *
+     * @param list
+     * @return
+     */
+    @InsertProvider(type = CRUDTemplate.class, method = "insertList")
+    <T> int insertList(@Param("list") List<T> list);
 
-	/**
-	 * 删除一个实体类
-	 *
-	 * @param obj
-	 */
-	@DeleteProvider(type = CRUDTemplate.class, method = "delete")
-	<T> int delete(T obj);
+    /**
+     * 删除一个实体类
+     *
+     * @param obj
+     */
+    @DeleteProvider(type = CRUDTemplate.class, method = "delete")
+    <T> int delete(T obj);
 
-	/**
-	 * 更新一个实体,根据ID更新。 <br>
-	 *
-	 * @param obj
-	 *            带ID的对象
-	 * @param ignoreNull
-	 *            是否忽略Null字段
-	 * @param ignoreEmpty
-	 *            是否忽略""字段
-	 * @return
-	 */
-	@UpdateProvider(type = CRUDTemplate.class, method = "update")
-	<T> int update(@Param("bean") T obj, @Param("ignoreNull") boolean ignoreNull, @Param("ignoreEmpty") boolean ignoreEmpty);
+    /**
+     * 更新一个实体,根据ID更新。 <br>
+     *
+     * @param obj 带ID的对象
+     * @param ignoreNull 是否忽略Null字段
+     * @param ignoreEmpty 是否忽略""字段
+     * @return
+     */
+    @UpdateProvider(type = CRUDTemplate.class, method = "update")
+    <T> int update(@Param("bean") T obj, @Param("ignoreNull") boolean ignoreNull, @Param("ignoreEmpty") boolean ignoreEmpty);
 
-	@InsertProvider(type = CRUDTemplate.class, method = "refactorSql")
-	int executeInsert(@Param("sql") String sql, @Param("params") Object... param);
+    @InsertProvider(type = CRUDTemplate.class, method = "refactorSql")
+    int executeInsert(@Param("sql") String sql, @Param("params") Object... param);
 
-	@DeleteProvider(type = CRUDTemplate.class, method = "refactorSql")
-	int executeDelete(@Param("sql") String sql, @Param("params") Object... param);
+    @DeleteProvider(type = CRUDTemplate.class, method = "refactorSql")
+    int executeDelete(@Param("sql") String sql, @Param("params") Object... param);
 
-	@UpdateProvider(type = CRUDTemplate.class, method = "refactorSql")
-	int executeUpdate(@Param("sql") String sql, @Param("params") Object... param);
+    @UpdateProvider(type = CRUDTemplate.class, method = "refactorSql")
+    int executeUpdate(@Param("sql") String sql, @Param("params") Object... param);
 }
--- wtyh-domain/src/main/java/com/bbd/wtyh/core/dao/GeneralMapper.java
+++ wtyh-domain/src/main/java/com/bbd/wtyh/core/dao/GeneralMapper.java
@@ -1,5 +1,6 @@
 package com.bbd.wtyh.core.dao;
 
+import com.bbd.wtyh.core.mybatis.annotation.ResultIntercept;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -8,8 +9,7 @@ import org.springframework.stereotype.Repository;
  * @author Created by LiYao on 2017-03-03 22:46.
  */
 @Repository
+@ResultIntercept
 public interface GeneralMapper extends BaseMapper {
-
-
 
 }
