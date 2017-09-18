9dbce833c3767e70708cad9fcc84668d41675362
sunliming <sunliming@bbdservice.com>
2017-09-18 09:54:00 +0800

    代码提交

--- wtyh-service/src/main/java/com/bbd/wtyh/service/impl/RelationServiceImpl.java
+++ wtyh-service/src/main/java/com/bbd/wtyh/service/impl/RelationServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.util.CollectionUtils;
 import java.util.List;
 
 /**
- * Created by Barney on 2017/7/12. test
+ * Created by Barney on 2017/7/12.
  */
 @Service
 public class RelationServiceImpl implements RelationService {
