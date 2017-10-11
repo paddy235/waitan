62e6eed4ee9d35e32b68d6fc5846ef0257a9b2a6
sunliming <sunliming@bbdservice.com>
2017-10-11 10:11:33 +0800

    企业批量变更	私募基金、典当、众筹、商业保理行业需要可以导入风险状态

--- wtyh-admin/src/main/java/com/bbd/bgo/service/imp/handler/company/modify/validator/PrivateFundValidator.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/service/imp/handler/company/modify/validator/PrivateFundValidator.java
@@ -1,6 +1,5 @@
 package com.bbd.bgo.service.imp.handler.company.modify.validator;
 
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * 私募基金校验器
@@ -25,9 +24,7 @@ public class PrivateFundValidator extends AbstractRiskValueValidator {
 
     @Override
     public String validate(String riskValue) {
-        if (StringUtils.isNotBlank(riskValue)) {
-            return "私募基金风险状态为不可填项，请删除";
-        }
+
         return null;
     }
 }
