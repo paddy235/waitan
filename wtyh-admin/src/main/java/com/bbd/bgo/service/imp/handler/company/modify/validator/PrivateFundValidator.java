301abdcdcd115e46e55c6a5a5aaf8e764646b12e
sunliming <sunliming@bbdservice.com>
2017-10-12 14:21:24 +0800

    典当、众筹、私募基金、商业保理   用线下理财的验证器

--- wtyh-admin/src/main/java/com/bbd/bgo/service/imp/handler/company/modify/validator/PrivateFundValidator.java
+++ wtyh-admin/src/main/java/com/bbd/bgo/service/imp/handler/company/modify/validator/PrivateFundValidator.java
@@ -1,45 +1,14 @@
 package com.bbd.bgo.service.imp.handler.company.modify.validator;
 
 
-import com.bbd.wtyh.constants.JYSCoRiskLevel;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * 私募基金校验器
  */
-public class PrivateFundValidator extends AbstractRiskValueValidator {
-
-    // do nothing
-    @Override
-    protected String[] riskDescArray() {
-        return null;
-    }
+public class PrivateFundValidator extends OfflineFinanceValidator {
 
     @Override
-    protected boolean contains(String riskValue) {
-        return true;
-    }
-
-    @Override
-    protected String msgPrefix() {
-        return null;
-    }
-
-    @Override
-    public String validate(String riskValue) {
-
-        return null;
-    }
-    @Override
-    public String convertDigit(String riskValue) {
-        if (StringUtils.isBlank(riskValue)) {
-            return riskValue;
-        }
-        for (JYSCoRiskLevel level : JYSCoRiskLevel.values()) {
-            if (level.desc().equals(riskValue)) {
-                return level.type() + "";
-            }
-        }
-        return riskValue;
+    public String getTypeName() {
+        return "私募基金";
     }
 }
