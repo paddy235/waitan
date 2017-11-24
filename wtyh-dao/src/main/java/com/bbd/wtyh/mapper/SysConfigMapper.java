bba6890b37389f84f9fa9f3f71aca292f5989ead
fuming <fm365482>
2017-11-24 14:09:40 +0800

    获取网页链接

---
+++ wtyh-dao/src/main/java/com/bbd/wtyh/mapper/SysConfigMapper.java
@@ -0,0 +1,19 @@
+package com.bbd.wtyh.mapper;
+
+import com.bbd.wtyh.domain.SysConfigDo;
+
+public interface SysConfigMapper {
+
+    /**
+     *根据key获取链接
+     */
+    String findByKey(String key);
+
+
+
+
+
+
+
+
+}
