ba9b0e9eb290fbbd1104df76708a2376ef378170
liangliang <weiliangliang@andlinks.com>
2017-12-04 16:25:21 +0800

    传参优化

--- wtyh-web/src/main/java/com/bbd/wtyh/web/controller/PrivateFundController.java
+++ wtyh-web/src/main/java/com/bbd/wtyh/web/controller/PrivateFundController.java
@@ -183,7 +183,7 @@ public class PrivateFundController {
 
 	//新增私募企业查询接口
 	@RequestMapping("privateFundQueryList.do")
-	public ResponseBean privateFundQueryList(String companyName,Integer orderByField,String descAsc,Integer recordStatus,@RequestParam(defaultValue = "10") Integer pageSize,
+	public ResponseBean privateFundQueryList(String companyName,Integer orderByField,String descAsc,@RequestParam(defaultValue = "")Integer recordStatus,@RequestParam(defaultValue = "10") Integer pageSize,
 											 @RequestParam(defaultValue = "1") Integer currentPage){
 		int start = (currentPage-1)*pageSize;
 		PageBean<PrivateFundCompanyDTO> pageInfo = privateFundService.privateFundQueryList(companyName,orderByField,descAsc,recordStatus,start,pageSize);
